150a7a50e55fffa82fbb76127b17251a14661373
Edolfo <edolfo@civinomics.com>
2012-03-06 00:17:08 -0800

    updated changePassword, checkPassword, hashPassword to work with new schema.  Added activationHash field to user creation.
    -Edolfo

--- pylowiki/lib/db/user.py
+++ pylowiki/lib/db/user.py
@@ -15,6 +15,19 @@ def get_user(name):
     except sa.orm.exc.NoResultFound:
         return False
 
+def changePassword(user, password):
+    user['password'] = hashPassword(password)
+    commit(user)
+    return True
+
+def checkPassword(user, password):
+    if user['password'] == hashPassword(password):
+        return True
+    return False 
+    
+def hashPassword(password):
+    return md5(password + config['app_conf']['auth.pass.salt']).hexdigest()
+
 class User(object):
     def __init__(self, firstName, lastName, email, password, zipCode = '00000'):
         u = Thing('user')
