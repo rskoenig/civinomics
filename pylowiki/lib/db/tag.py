2eff756f2eb1febb18b9501fe2e8147071c084cf
Chris Neklason <chris@civinomics.com>
2012-04-27 21:31:27 -0700

    Added tag counts for tag clouds, and tag listss for tag based searches

--- pylowiki/lib/db/tag.py
+++ pylowiki/lib/db/tag.py
@@ -3,13 +3,55 @@ import logging
 
 from pylowiki.model import Thing, Data, meta
 import sqlalchemy as sa
-from dbHelpers import commit, with_characteristic
+from dbHelpers import commit, with_characteristic as wc
 
 def getWorkshopTags(workshopID):
     try:
         return meta.Session.query(Thing).filter_by(objType = 'tag').filter(Thing.data.any(wc('thingID', workshopID))).all()
     except:
         return False
+
+def getPublicTagList():
+    pTagList = []
+    pTagList.append('Environment')
+    pTagList.append('Government')
+    pTagList.append('Municipal Services')
+    pTagList.append('Economy')
+    pTagList.append('Infrastructure')
+    pTagList.append('Civil Rights')
+    pTagList.append('Civic Response')
+    pTagList.append('Business')
+    return pTagList
+
+def getPublicTagCount():
+    tagDict = dict()
+    tL = getPublicTagList()
+    for pT in tL:
+       tagDict[pT] = meta.Session.query(Thing).filter_by(objType = 'tag').filter(Thing.data.any(wc('tagType', 'system'))).filter(Thing.data.any(wc('tagName', pT))).count()
+
+    return tagDict
+
+def getMemberTagList():
+    tSearch =  meta.Session.query(Thing).filter_by(objType = 'tag').filter(Thing.data.any(wc('tagType', 'member'))).all()
+
+    tagList = ()
+    for tL in tSearch:
+       if tL['tagName'] not in tagList:
+          tagList.append(tL['tagName'])
+
+    return tagList
+
+def getMemberTagCount():
+    tSearch =  meta.Session.query(Thing).filter_by(objType = 'tag').filter(Thing.data.any(wc('tagType', 'member'))).all()
+    tagDict = dict()
+    for tL in tSearch:
+       t = tL['tagName']
+       if t not in tagDict:
+           tagDict[t] = 1
+       else:
+           tagDict[t] += 1
+
+    return tagDict
 
 class Tag(object):
     def __init__(self, tagType, tagName, thingID, ownerID):
