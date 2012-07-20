c6452f1794b9cde0fec8db181bc2a4ba5f775eba
edolfo <edolfo@localhost>
2012-07-20 15:36:49 -0700

    Backed out changeset: 8f1b25957aeb

---
+++ pylowiki/controllers/actionlist.py
@@ -0,0 +1,223 @@
+# -*- coding: utf-8 -*-
+import logging
+
+from pylons import request, response, session, tmpl_context as c, url
+from pylons.controllers.util import abort, redirect
+
+from pylowiki.lib.base import BaseController, render
+
+from pylowiki.lib.db.page import get_all_pages
+from pylowiki.lib.db.workshop import getActiveWorkshops, searchWorkshops, getWorkshopByID
+from pylowiki.lib.db.survey import getActiveSurveys, getSurveyByID
+from pylowiki.lib.db.tag import searchTags
+from pylowiki.lib.db.user import searchUsers, getUserByID
+from pylowiki.lib.db.geoInfo import getGeoInfo, getUserScopes, getWorkshopScopes
+from pylowiki.lib.db.featuredSurvey import getFeaturedSurvey, setFeaturedSurvey
+
+import webhelpers.paginate as paginate
+import pylowiki.lib.helpers as h
+from pylons import config
+
+log = logging.getLogger(__name__)
+
+class ActionlistController(BaseController):
+
+    def __before__(self):
+        if c.conf['public.sitemap'] != "true": 
+            h.check_if_login_required()
+
+    
+    def index( self, id ): # id is the action
+        """Create a list of pages with the given action/option """
+        """Valid actions: edit, revision, delete, restore, sitemap """
+        c.action = id
+        if c.action == "sitemap":
+            c.title = c.heading = c.action
+            c.action = ""
+            c.list = get_all_pages()
+        elif c.action == 'sitemapIssues':
+            c.title = c.heading = 'Workshops'
+            c.list = getActiveWorkshops()
+        elif c.action == 'surveys':
+            c.title = c.heading = 'Surveys'
+            c.list = getActiveSurveys()
+        else:
+            c.title = c.heading = "Which " + c.action + "?"
+
+        if c.action == "restore":
+            c.list = get_all_pages(1)
+
+        if 'user' in session:
+            if not c.authuser:
+                session.delete()
+                return redirect('/')
+            items = []
+            userZip = int(c.authuser['postalCode'])
+            for item in c.list:
+                itemZip = map(int, item['publicPostalList'].split(','))
+                if userZip in itemZip:
+                    items.append(item)
+            c.list = items
+            
+        c.count = len( c.list )
+        c.paginator = paginate.Page(
+            c.list, page=int(request.params.get('page', 1)),
+            items_per_page = 10, item_count = c.count
+        )
+
+        # Hack to get around undiscussed conflicts in 'featured' item 
+        # listing with respect to geographic scoping
+        if len(c.list) >= 1:
+            featuredSurvey = c.list.pop()
+            c.mainSurvey = featuredSurvey
+            c.surveys = c.list
+        """
+        if len(c.list) >= 1:
+            featuredSurvey = getFeaturedSurvey()
+            if not featuredSurvey:
+                setFeaturedSurvey(c.list[0])
+                c.mainSurvey = c.list[0]
+                c.surveys = c.list[1:]
+            else:
+                featuredSurveyID = int(featuredSurvey['survey'])
+                featuredSurvey = getSurveyByID(featuredSurveyID)
+                log.info(featuredSurvey['active'])
+                if int(featuredSurvey['active']) == 0:
+                    c.mainSurvey = None
+                else:
+                    c.mainSurvey = featuredSurvey
+                for i in range(len(c.list)):
+                    if c.list[i].id == featuredSurveyID:
+                        c.list.pop(i)
+                        break
+                c.surveys = c.list
+        else:
+            c.mainSurvey = []
+          """
+        return render('/derived/list_surveys.bootstrap')
+
+    def searchWorkshops( self, id1, id2  ):
+        log.info('searchWorkshops %s %s' % (id1, id2))
+        id2 = id2.replace("_", " ")
+        c.title = c.heading = 'Search Workshops: ' + id1 + ' ' + id2
+        c.list = searchWorkshops(id1, id2)
+        c.count = len( c.list )
+        c.paginator = paginate.Page(
+            c.list, page=int(request.params.get('page', 1)),
+            items_per_page = 10, item_count = c.count
+        )
+
+        return render('/derived/list_workshops.html')
+
+    def searchName( self ):
+        log.info('searchName')
+        if 'searchType' in request.params and 'searchString' in request.params:
+           searchType = request.params['searchType']
+           searchString = request.params['searchString']
+
+           if searchType == 'Workshops':
+              c.title = c.heading = 'Search Workshops: ' + searchString
+              c.list = searchWorkshops('title', searchString)
+              c.count = len( c.list )
+              c.paginator = paginate.Page(
+                  c.list, page=int(request.params.get('page', 1)),
+                  items_per_page = 10, item_count = c.count
+              )
+
+              return render('/derived/list_workshops.html')
+
+           else:
+              c.title = c.heading = 'Search Members: ' + searchString
+              c.list = searchUsers('name', searchString)
+              c.count = len( c.list )
+              c.paginator = paginate.Page(
+                  c.list, page=int(request.params.get('page', 1)),
+                  items_per_page = 10, item_count = c.count
+              )
+
+              return render('/derived/list_users.html')
+        else:
+           return redirect('/')
+
+    def searchGeoUsers( self ):
+        log.info('searchGeoUsers')
+        c.title = c.heading = 'List Nearby Members'
+        geoInfo = getGeoInfo(c.authuser.id)
+        searchScope = geoInfo[0]['scope']
+        log.info('geoInfo is %s'%geoInfo)
+        c.list = []
+        if 'scopeLevel' in request.params:
+           scopeLevel = request.params['scopeLevel']
+           scopeList = getUserScopes(searchScope, scopeLevel)
+           for gInfo in scopeList:
+              c.list.append(getUserByID(gInfo.owner))
+
+           c.count = len( c.list )
+           c.paginator = paginate.Page(
+                     c.list, page=int(request.params.get('page', 1)),
+                     items_per_page = 10, item_count = c.count
+                 )
+
+           return render('/derived/list_users.html')
+        else:
+           return redirect('/')
+
+    def searchGeoWorkshops( self ):
+        log.info('searchGeoWorkshops')
+        c.title = c.heading = 'List Nearby Workshops'
+        geoInfo = getGeoInfo(c.authuser.id)
+        searchScope = geoInfo[0]['scope']
+        log.info('geoInfo is %s'%geoInfo)
+        c.list = []
+        if 'scopeLevel' in request.params:
+           scopeLevel = request.params['scopeLevel']
+           scopeList = getWorkshopScopes(searchScope, scopeLevel)
+           for gInfo in scopeList:
+              w = getWorkshopByID(gInfo['workshopID'])
+              if w['startTime'] != '0000-00-00' and w['deleted'] != '1':
+                  if w not in c.list:
+                      c.list.append(w)
+
+           c.count = len( c.list )
+           c.paginator = paginate.Page(
+                     c.list, page=int(request.params.get('page', 1)),
+                     items_per_page = 10, item_count = c.count
+                 )
+
+           return render('/derived/list_workshops.html')
+        else:
+           return redirect('/')
+
+
+    def searchTags( self, id1 ):
+        log.info('searchTags %s' % id1)
+        id1 = id1.replace("_", " ")
+        c.title = c.heading = 'Search Workshops by Tag: ' + id1
+        tList = searchTags(id1)
+        log.info('tList %s' % tList)
+        c.list = []
+        for t in tList:
+           log.info('t %s' % t)
+           c.list.append(getWorkshopByID(t['thingID']))
+
+        c.count = len( c.list )
+        c.paginator = paginate.Page(
+            c.list, page=int(request.params.get('page', 1)),
+            items_per_page = 10, item_count = c.count
+        )
+
+        return render('/derived/list_workshops.html')
+
+    def searchUsers( self, id1, id2  ):
+        log.info('searchUsers %s %s' % (id1, id2))
+        id2 = id2.replace("_", " ")
+        c.title = c.heading = 'Search Users: ' + id1 + ' ' + id2
+        c.list = searchUsers(id1, id2)
+        c.count = len( c.list )
+        c.paginator = paginate.Page(
+            c.list, page=int(request.params.get('page', 1)),
+            items_per_page = 10, item_count = c.count
+        )
+
+        return render('/derived/list_users.html')
+
---
+++ pylowiki/controllers/wiki.py
@@ -0,0 +1,186 @@
+# -*- coding: utf-8 -*-
+import logging
+
+from pylons import request, response, session, tmpl_context as c, url
+from pylons.controllers.util import abort, redirect
+
+from pylowiki.lib.base import BaseController, render
+
+#fox added following imports
+#from pylowiki.model import commit_edit, get_page, get_all_pages
+from pylowiki.lib.db.page import get_page, get_all_pages, getPageByID
+from pylowiki.lib.db.workshop import getWorkshop
+from pylowiki.lib.db.revision import Revision
+from pylowiki.lib.db.dbHelpers import commit
+
+import pylowiki.lib.helpers as h
+import re
+
+log = logging.getLogger(__name__)
+
+class WikiController(BaseController):
+
+    def index(self, id):
+        """ Loads view or section edit page data for URL/ID depending on login status """
+        
+        c.p = get_page(id)
+        if c.p == False: # Error page not found
+            abort(404, h.literal("This page does not exist! Would you like to <a href='/create/assist/" + id + "'> Create it?</a>") )
+        
+        c.title = c.p.url
+        c.url = c.p.url 
+    
+        r = c.p.revisions[0]
+
+        c.lastmoddate = r.event.date
+        c.lastmoduser = r.event.user.name
+
+        if session.get('user'): # if use in session load wiki logic else just view the page
+            reST = r.data
+
+            reSTlist = self.get_reSTlist(reST) # function below
+            HTMLlist = self.get_HTMLlist(reST) # function below 
+
+            #print "HTML rows " + str(len( HTMLlist ))
+            #print "reST rows " + str(len( reSTlist ))
+        
+            c.wikilist = zip ( HTMLlist, reSTlist ) # link the lists (a list of lists) 
+            return render( '/derived/wiki.mako' )
+        else:
+            c.content = h.literal(h.reST2HTML(r.data))
+            return render('/derived/view.mako')
+
+    def readThisArticle(self):
+        readArticle(c.authuser.id)
+        numPoints = c.authuser.pointsObj.points
+        if numPoints % 10 == 0:
+            h.flash('Congratulations on winning the %d points award!' % numPoints)
+        return redirect('/' + c.url) 
+    
+    def random( self ):
+        import random   
+        pagelist = get_all_pages()
+        pagelistlen = len( pagelist ) - 1
+        rand = random.randint(0, pagelistlen)
+        pageurl = pagelist[rand].url
+        redirect( "/" + pageurl )
+        
+
+    @h.login_required   
+    def handler(self, id1, id2):
+        """ Handles wiki Submit """
+        code = id1
+        url = id2
+        
+        try:
+
+            request.params['submit'] #Try submit, if false redirect back.
+            data = remark = ""
+            for i in range( 0, int(request.params['count'])): # Get all the textarea content, append to data
+                data = data + request.params['textarea'+str(i)]
+                if request.params.get('remark'+str(i)) is None:
+                    pass # Do nothing
+                else:
+                    remark = remark + request.params.get('remark'+str(i))
+            
+            w = getWorkshop(code, url)
+            if not w:
+                h.flash('Workshop not found', 'error')
+                return redirect('/')
+            
+            page = getPageByID(w['page_id'])
+            r = Revision(c.authuser, data, page)
+            w['mainRevision_id'] = r.r.id
+            commit(w)
+            
+            """
+            if commit_edit( id, session['user'], data, "edit", remark ):
+                h.flash( "The page was saved!", "success" )
+            else:
+                h.flash( "The page was NOT saved.", "warning" )
+            """
+        except KeyError: 
+            h.flash( "Do not access a handler directly", "error" )
+            
+        return redirect( "/workshops/%s/%s/background" %(code, url) )
+    
+    @h.login_required
+    def previewer( self, id ):
+        """ AJAX Handler - Accepts reST returns HTML """
+        try:
+            preview = h.literal(h.reST2HTML(request.params['data']))
+        except:
+            preview = "there was a problem with ajax..."
+        return preview
+
+
+# ------------------------------------------
+#    Helper functions for wiki controller
+#-------------------------------------------
+
+    @h.login_required
+    def CleanList( self, l ):
+        """ Remove all empty rows from list """
+        counter = 0
+        length = len( l )
+        while counter < length:
+            if l[counter].isspace() or l[counter] == None or l[counter] == "":
+                del l[counter]
+                length = len( l )   
+            counter += 1
+        return l
+
+
+    @h.login_required
+    def isodd( self, integer ):
+        """ if interger is odd return True, else return False"""
+        if integer % 2 == 0:
+            return False
+        else:
+            return True
+
+
+    @h.login_required
+    def get_reSTlist( self, reST ):
+        """Accept a reST string and return a list of sections
+        This code is a bit ugly but it works... 
+        SORRY IF YOU HAVE TO WORK ON THIS..."""
+
+        splitter = re.compile('(.*\r\n[=\-`:~^_*+#]{3,}.+\r\n+)') # Create regex object that splits reST string by headings
+        reSTlist = self.CleanList(splitter.split( reST )) # Split the reST into a list, and remove any empty rows
+        
+        """reSTlist has heading and section data in seperate rows.
+        The logic below merges the heading and section data rows."""
+        
+        lenght = len( reSTlist )
+        
+        if self.isodd( lenght ): # if the lenght is odd, the first row doesn't have a heading and will be in its own section
+            offset = counter = 1
+        else:
+            offset = counter = 0
+
+        """ join the heading list row with the data list row """
+        while counter < lenght: # loop through list until end
+            if self.isodd( counter + offset ): # do nothing if even
+                reSTlist[counter-1] = reSTlist[counter-1] + reSTlist[counter]
+                reSTlist[counter] = "" # set current row to empty
+            counter = counter + 1
+
+        return self.CleanList( reSTlist ) # remove empty rows and return list
+
+
+    @h.login_required
+    def get_HTMLlist( self, reST):
+        """Accept reST list, convert to html, remove newlines so regex works, split HTML into a list by heading."""
+
+        splitter = re.compile('(<div class\="section".*?)(?=<div class\="section")', re.DOTALL) # Create regex object to split HTML by header section.
+        HTMLlist = self.CleanList(splitter.split(h.literal(h.reST2HTML( reST ))))
+        
+        if len(HTMLlist) == 1: # This only happens if there are two sections but only one heading
+            splitter = re.compile('(<div class\="section".*</div>)', re.DOTALL) # Create regex object to split HTML by header section.
+            HTMLlist = self.CleanList(splitter.split(h.literal(h.reST2HTML( reST ))))      
+    
+        return HTMLlist
+
+            
+        
