cb3316360cfd2e50284242f5142af700982b4d3f
edolfo <edolfo@localhost>
2012-07-20 15:42:46 -0700

    Backed out merge changeset: 9eb2caa3db3e
    Backed out merge revision to its first parent (870ee4a1a330)

--- pylowiki/controllers/geo.py
+++ pylowiki/controllers/geo.py
@@ -1,9 +1,8 @@
 import logging
 
 from pylons import request, response, session, tmpl_context as c
-from string import capwords
 from pylowiki.lib.utils import urlify
-from pylowiki.lib.db.geoInfo import geoDeurlify, getPostalInfo, getCityInfo, getCountyInfo, getStateInfo, getGeoScope, getWorkshopScopes
+from pylowiki.lib.db.geoInfo import geoDeurlify, getPostalInfo, getCityInfo, getCountyInfo, getStateInfo
 
 from pylowiki.lib.base import BaseController, render
 import pylowiki.lib.helpers as h
@@ -21,15 +20,7 @@ class GeoController(BaseController):
         c.heading = 'Civinomics: ' + c.country + ' ' + c.postal + ' Information'
         
         c.postalInfo = getPostalInfo(c.postal, c.country)
-        c.city = capwords(c.postalInfo['City'])
-        c.cityFlag = '/images/flags/country/united-states/city_thumb.png'
-        c.county = capwords(c.postalInfo['County'])
-        c.countyFlag = '/images/flags/country/united-states/county_thumb.png'
-        c.state = capwords(c.postalInfo['StateFullName'])
-        c.stateFlag = '/images/flags/country/united-states/states/' + urlify(c.state) + '_thumb.gif'
-        scope = getGeoScope(c.postal, c.country)
-        c.wscopes = getWorkshopScopes(scope, 9)
-        return render('/derived/postalInfo.bootstrap')
+        return render('/derived/postalinfo.mako')
 
     def showCityInfo(self, id1, id2, id3):
         c.country = geoDeurlify(id1)
@@ -39,15 +30,7 @@ class GeoController(BaseController):
         c.heading = 'Civinomics: ' + c.country + ' City of ' + c.city + ' Information'
         
         c.cityInfo = getCityInfo(c.city, c.state, c.country)
-        c.city = capwords(c.city)
-        c.cityFlag = '/images/flags/country/united-states/city_thumb.png'
-        c.county = capwords(c.cityInfo['County'])
-        c.countyFlag = '/images/flags/country/united-states/county_thumb.png'
-        c.stateFlag = '/images/flags/country/united-states/states/' + urlify(c.state) + '_thumb.gif'
-        c.state = capwords(c.cityInfo['StateFullName'])
-        scope = '||' + urlify(c.country) + '||' + urlify(c.state) + '||' + urlify(c.county) + '||' + urlify(c.city) + '|' +  '00000'
-        c.wscopes = getWorkshopScopes(scope, 8)
-        return render('/derived/cityInfo.bootstrap')
+        return render('/derived/cityinfo.mako')
 
     def showCountyInfo(self, id1, id2, id3):
         c.country = geoDeurlify(id1)
@@ -57,24 +40,14 @@ class GeoController(BaseController):
         c.heading = 'Civinomics: ' + c.country + ' County of ' + c.county + '  Information'
         
         c.countyInfo = getCountyInfo(c.county, c.state, c.country)
-        c.county = capwords(c.countyInfo['County'])
-        c.countyFlag = '/images/flags/country/united-states/county_thumb.png'
-        c.stateFlag = '/images/flags/country/united-states/states/' + urlify(c.state) + '_thumb.gif'
-        c.state = capwords(c.countyInfo['StateFullName'])
-        scope = '||' + urlify(c.country) + '||' + urlify(c.state) + '||' + urlify(c.county) + '||' + 'LaLaLa|00000'
-        c.wscopes = getWorkshopScopes(scope, 6)
-        return render('/derived/countyInfo.bootstrap')
+        return render('/derived/countyinfo.mako')
 
     def showStateInfo(self, id1, id2):
         c.country = geoDeurlify(id1)
         c.state = geoDeurlify(id2)
         
         c.heading = 'Civinomics: ' + c.country + ' State of ' + c.state + ' Information'
-        c.stateInfo = getStateInfo(c.state, c.country)
-        c.stateFlag = '/images/flags/country/united-states/states/' + urlify(c.state) + '_thumb.gif'
-        c.state = capwords(c.stateInfo['StateFullName'])
-        scope = '||' + urlify(c.country) + '||' + urlify(c.state) + '||' + 'LaLaLa||LaLaLa|00000'
-        c.wscopes = getWorkshopScopes(scope, 4)
         
-        return render('/derived/stateInfo.bootstrap')
+        c.stateInfo = getStateInfo(c.state, c.country)
+        return render('/derived/stateinfo.mako')
 
