67c67c322fdd0a8410c880432c67d90a845a342c
todda <todda@civinomics.com>
2012-11-16 11:39:16 -0800

    test_account.py has a new test, better assertions and comments

--- pylowiki/tests/functional/test_account.py
+++ pylowiki/tests/functional/test_account.py
@@ -29,6 +29,16 @@ class TestAccountController(TestController):
     userName2AdminButton = str.lower(first2) + '-' + str.lower(last2) + '/admin'
     user2AccessLevel = 'facilitator'
 
+    user3 = 'test3@civinomics.org'
+    pass3 = 'test3'
+    zip3 = '94122'
+    member3 = 'individual'
+    first3 = 'Testing3'
+    last3 = 'Home3'
+    userName3 = first3 + ' ' + last3
+    userName3AdminButton = str.lower(first3) + '-' + str.lower(last3) + '/admin'
+    user3AccessLevel = 'admin'
+
     adminEmail = 'username@civinomics.com'
     adminPass = 'password'
     adminController = 'systemAdmin'
@@ -40,6 +50,8 @@ class TestAccountController(TestController):
     select_num_objects_name = 'numHost'
     num_objects_to_host = '3'
 
+    # This test will create a user then log in as an admin and view this user's account admin page. Then we will 
+    # use the hosted accounts form to update their hosted account number and confirm that it worked on the resulting page.
     def test_change_hosted_accounts_admin(self):
         # create a user
         create_and_activate_user(self, TestAccountController.user1, TestAccountController.pass1, TestAccountController.zip1, TestAccountController.member1,TestAccountController.first1, TestAccountController.last1)
@@ -53,10 +65,21 @@ class TestAccountController(TestController):
         hosted_accounts_updated = admin_page_account_form.submit().follow()
         # we should see success by noting the page now states this many hosted accounts are now hosted.
         assert "Total hosting for account: " + TestAccountController.num_objects_to_host in hosted_accounts_updated
+        assert "Number of objects which may be hosted:" not in hosted_accounts_updated
     
+    # This test will have a facilitator try to spoof the hosted account handler, telling it to update 
+    # the hosted accounts object. After doing this, we log in as an admin and take a look at this user's account
+    # admin page. The assertions at the end of this function explain what we should and should not see.
     def test_change_hosted_accounts_facilitator(self):
         # create a user
-        create_and_activate_user(self, TestAccountController.user2, TestAccountController.pass2, TestAccountController.zip2, TestAccountController.member2,TestAccountController.first2, TestAccountController.last2)
+        create_and_activate_user(self, 
+            TestAccountController.user2, 
+            TestAccountController.pass2, 
+            TestAccountController.zip2, 
+            TestAccountController.member2, 
+            TestAccountController.first2, 
+            TestAccountController.last2
+        )
         # set this user's access level to facilitator
         adminPrivsRes = login_and_set_user_auth_level(self, TestAccountController.adminEmail, TestAccountController.adminPass, TestAccountController.adminController, TestAccountController.adminAction, TestAccountController.first2, TestAccountController.last2, TestAccountController.user2AccessLevel)
         # logout
@@ -77,9 +100,66 @@ class TestAccountController(TestController):
         response = self.app.post(
             url=url(controller='account', action='accountAdminHandler', id1=this_code, id2=this_name), 
             params={
-                'numHost' : '3'
+                TestAccountController.select_num_objects_name : TestAccountController.num_objects_to_host
             }
         )
         landing = response.follow()
-        assert "Total hosting for account: " + TestAccountController.num_objects_to_host not in landing
-        assert landing.status_int == 302
+        # if it works, the database will reflect this new numHost value. login as admin and take a look
+        logout(self)
+        user_admin_page = login_and_view_user_admin_page(self, TestAccountController.adminEmail, TestAccountController.adminPass, TestAccountController.adminController, TestAccountController.adminAction, TestAccountController.first2, TestAccountController.last2)
+        # since this was a facilitator trying to set hosting accounts, this should not have gone through.
+        # Therefore, the previous test string will not be there and instead the one that prints when no 
+        # hosted accounts are set will be:
+
+        assert "Number of objects which may be hosted:" in user_admin_page
+        assert "Total hosting for account: " + TestAccountController.num_objects_to_host not in user_admin_page
+        assert user_admin_page.status_int == 200
+
+    # This test will confirm the previous one would have worked if the user would have been set as an admin. This user 
+    # sends the same post at the hosted account handler, telling it to update the hosted accounts object.
+    # After doing this, we log in as an admin and take a look at this user's account
+    # admin page. The assertions at the end of this function are opposite of the previous test.
+    def test_change_hosted_accounts_new_admin(self):
+        # create a user
+        create_and_activate_user(self, 
+            TestAccountController.user3, 
+            TestAccountController.pass3, 
+            TestAccountController.zip3, 
+            TestAccountController.member3, 
+            TestAccountController.first3, 
+            TestAccountController.last3
+        )
+        # set this user's access level to facilitator
+        adminPrivsRes = login_and_set_user_auth_level(self, TestAccountController.adminEmail, TestAccountController.adminPass, TestAccountController.adminController, TestAccountController.adminAction, TestAccountController.first3, TestAccountController.last3, TestAccountController.user3AccessLevel)
+        # logout
+        logout(self)
+        # login as this new user
+        login = login_user(self, TestAccountController.user3, TestAccountController.pass3)
+        # visit my profile page and obtain code/name
+        landing_page = self.app.get(url=url(controller='actionlist', action='index', id='sitemapIssues'))
+        profile_page = landing_page.click("Profile")
+        # profile_page.request.url == http://localhost/profile/4ICk/testing3-home3
+        profile_url = profile_page.request.url
+        pile = profile_url.split('/')
+        name_index = len(pile)-1
+        code_index = len(pile)-2
+        this_name = pile[name_index]
+        this_code = pile[code_index]
+        # use this along with spoofed form data and send at the handler 
+        response = self.app.post(
+            url=url(controller='account', action='accountAdminHandler', id1=this_code, id2=this_name), 
+            params={
+                TestAccountController.select_num_objects_name : TestAccountController.num_objects_to_host
+            }
+        )
+        landing = response.follow()
+        # if it works, the database will reflect this new numHost value. login as admin and take a look
+        logout(self)
+        user_admin_page = login_and_view_user_admin_page(self, TestAccountController.adminEmail, TestAccountController.adminPass, TestAccountController.adminController, TestAccountController.adminAction, TestAccountController.first3, TestAccountController.last3)
+        # since this was a facilitator trying to set hosting accounts, this should not have gone through.
+        # Therefore, the previous test string will not be there and instead the one that prints when no 
+        # hosted accounts are set will be:
+
+        assert "Number of objects which may be hosted:" not in user_admin_page
+        assert "Total hosting for account: " + TestAccountController.num_objects_to_host in user_admin_page
+        assert user_admin_page.status_int == 200
