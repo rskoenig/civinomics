161b0aa486e4195ad0148887c32c077e4daf67b2
edolfo <edolfo@civinomics.com>
2013-01-17 18:02:16 -0800

    Reworked and simplified wiki stuff for workshop's background information.  Updated workshop creation process to reflect the changes.  Beginning of workshop listing design additions, but should be ignored for now.  This commit will likely require a DB reset, due to the large changes made in object references.

---
+++ pylowiki/lib/db/generic.py
@@ -0,0 +1,42 @@
+import logging
+log = logging.getLogger(__name__)
+
+from pylowiki.model import Thing, Data, meta
+from dbHelpers import with_characteristic as wc
+
+def linkChildToParent(child, parent):
+    # Defines a standard for object linking.
+    # All objects that get accessed through a URL need the following two fields:
+    # 
+    # urlCode: obtained from toBase62() in pylowiki/lib/utils.py
+    # url: obtained from urlify() in pylowiki/lib/utils.py
+    # 
+    # Because the urlCode field is unique, this is what we use to link objects.
+    try:
+        code = parent['urlCode']
+    except Exception as e:
+        log.error("linkChildToParent(): parent object missing 'urlCode' field.")
+        return False
+    
+    key = '%s%s' %(parent.objType, 'Code')
+    if key not in child:
+        child[key] = code
+    else:
+        log.warning("linkChildToParent(): parent object link already exists in child.")
+    return child
+   
+def getThing(code):
+    try:
+        return meta.Session.query(Thing)\
+            .filter(Thing.data.any(wc('urlCode', code))).one()
+    except Exception as e:
+        log.info(e)
+        return False
+        
+def getThingByID(thingID):
+    try:
+        return meta.Session.query(Thing)\
+            .filter_by(id = thingID)\
+            .one()
+    except:
+        return False
