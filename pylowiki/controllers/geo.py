f9686450f4abcb075ec0fbe614cb01fa06432cc5
edolfo <edolfo@civinomics.com>
2013-01-25 18:22:19 -0800

    Removed unused code.  Updated workshopSearch function parameters to be consistent.

--- pylowiki/controllers/geo.py
+++ pylowiki/controllers/geo.py
@@ -5,7 +5,6 @@ from pylons.controllers.util import abort, redirect
 from pylowiki.lib.base import BaseController, render
 import pylowiki.lib.helpers as h
 
-import pylowiki.lib.utils           as utils
 import pylowiki.lib.db.geoInfo      as geoInfoLib
 import pylowiki.lib.db.workshop     as workshopLib
 
@@ -25,27 +24,21 @@ class GeoController(BaseController):
         
         if country == '0':
             c.scope = 'planet'
-            scopeLevel = '01'
             location = 'earth'
         elif state == '0':
             c.scope = 'country'
-            scopeLevel = '03'
             location = country
         elif county == '0':
             c.scope = 'state'
-            scopeLevel = '05'
             location = state
         elif city == '0':
             c.scope = 'county'
-            scopeLevel = '07'
             location = county
         elif postalCode == '0':
             c.scope = 'city'
-            scopeLevel = '09'
             location = city
         else:
             c.scope = 'postalCode'
-            scopeLevel = '10'
             location = postalCode
         c.title += capwords(geoInfoLib.geoDeurlify(location))
         c.heading += capwords(geoInfoLib.geoDeurlify(location))
@@ -65,7 +58,7 @@ class GeoController(BaseController):
                 c.list.append(workshop)
         c.activity = []
 
-    def workshopSearch(self, planet = None, country = None, state = None, county = None, city = None, postalCode = None):
+    def workshopSearch(self, planet = '0', country = '0', state = '0', county = '0', city = '0', postalCode = '0'):
         return render('derived/6_main_listing.bootstrap')
 
     ######################################################################
--- pylowiki/lib/utils.py
+++ pylowiki/lib/utils.py
@@ -61,28 +61,4 @@ def isWatching(user, workshop):
    # object follows as 'watching'.
    c.isFollowing = followLib.isFollowing(user, workshop)
   
-def constructGeoScope(**kwargs):
-    # scope = '||' + urlify(c.country) + '||LaLa||LaLaLa||LaLaLa|00000'
-    country = ''
-    state = 'LaLa'
-    county = 'LaLaLa'
-    city = 'LaLaLa'
-    postalCode = '00000'
-    if 'country' in kwargs:
-        if kwargs['country'] is not None:
-            country = string.capwords(geoDeurlify(kwargs['country']))
-    if 'state' in kwargs:
-        if kwargs['state'] is not None:
-            state = string.capwords(geoDeurlify(kwargs['state']))
-    if 'county' in kwargs:
-        if kwargs['county'] is not None:
-            county = string.capwords(geoDeurlify(kwargs['county']))
-    if 'city' in kwargs:
-        if kwargs['city'] is not None:
-            city = string.capwords(geoDeurlify(kwargs['city']))
-    if 'postalCode' in kwargs:
-        if kwargs['postalCode'] is not None:
-            postalCode = kwargs['postalCode']
-    
-    return '||' + '||'.join([country, state, county, city]) + '|' + postalCode
     
