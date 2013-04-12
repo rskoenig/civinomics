977ada78508fea4f7fb22f5cb9ed542b021bd09f
edolfo <edolfo@civinomics.com>
2013-04-11 22:27:26 -0700

    Merge branch 'development' into branch 'release'

---
+++ setup.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup, find_packages
+
+setup(
+    name='Pylowiki',
+    version='0.1',
+    description='',
+    author='',
+    author_email='',
+    url='',
+    install_requires=[
+        "Pylons>=0.9.7",
+        "SQLAlchemy==0.6.5",
+        #"SQLAlchemy>=0.6.6",
+        "docutils>=0.6",
+        "MySQL-python",
+        "Pygments",
+    ],
+    setup_requires=["PasteScript>=1.6.3"],
+    packages=find_packages(exclude=['ez_setup']),
+    include_package_data=True,
+    test_suite='nose.collector',
+    package_data={'pylowiki': ['i18n/*/LC_MESSAGES/*.mo']},
+    #message_extractors={'pylowiki': [
+    #        ('**.py', 'python', None),
+    #        ('templates/**.mako', 'mako', {'input_encoding': 'utf-8'}),
+    #        ('public/**', 'ignore', None)]},
+    zip_safe=False,
+    paster_plugins=['PasteScript', 'Pylons'],
+    entry_points="""
+    [paste.app_factory]
+    main = pylowiki.config.middleware:make_app
+
+    [paste.app_install]
+    main = pylons.util:PylonsInstaller
+    """,
+)
