5153de339d2e155736efc61b079d7dfc08743133
edolfo <edolfo@civinomics.com>
2013-04-19 13:57:20 -0700

    Search partially tested as working - searching for items that exist yield those items.

--- pylowiki/tests/functional/test_search.py
+++ pylowiki/tests/functional/test_search.py
@@ -4,8 +4,10 @@ log = logging.getLogger(__name__)
 
 import pylowiki.tests.helpers.registration as registration
 import pylowiki.tests.helpers.workshops as workshop
+import pylowiki.tests.helpers.authorization as authLib
+import pylowiki.tests.helpers.noun_verb_actions as nounAction
 
-import random, string
+import random, string, simplejson as json
 
 class TestSearchController(TestController):
 
@@ -36,15 +38,84 @@ class TestSearchController(TestController):
                             'ideas':'/search/ideas/'}
         
         searchRoute = searchRoutes[objType] + '?searchQuery=' + query
-        response = self.app.get(searchRoute)
-        log.debug('Search query was %s, response was %s' %(query, response))
+        #response = self.app.get(searchRoute)
+        return self.app.post_json(searchRoute)
     
     def searchWorkshop(self):
         return self.searchPrototype('workshops', 'test')
         
     def allTests(self):
-        thisUser = registration.create_and_activate_a_user(self, postal='95864', name=self._generateString(10))
-        thisWorkshop = workshop.create_new_workshop(self, thisUser, title=self._generateString(20))
+        seededValues = self._seedDB()
+        searchTypeMapping = {   'usernames': 'people',
+                                'workshopTitles': 'workshops',
+                                'resourceTitles': 'resources',
+                                'resourceTexts': 'resources',
+                                'ideaTitles': 'ideas',
+                                'discussionTitles': 'discussions',
+                                'discussionTexts': 'discussions'}
+        for key in seededValues.keys():
+            searchType = searchTypeMapping[key]
+            for value in seededValues[key]:
+                response = self.searchPrototype(searchType, value)
+                if response.status_int == 200:
+                    statusCode = json.loads(response.body)
+                    if statusCode != 0:
+                        return False
+                else:
+                    return False
+        return True
+    
+    def _seedDB(self):
+        # One user per workshop
+        numUsers = 3
+        usernames = [self._generateString(10) for i in range(numUsers)]
+        workshopTitles = [self._generateString(20).strip() for i in range(numUsers)]
+        resourceTitles = [self._generateString(20).strip() for i in range(numUsers)]
+        resourceTexts = [self._generateString(200).strip() for i in range(numUsers)]
+        ideaTitles = [self._generateString(20).strip() for i in range(numUsers)]
+        ideaTexts = [self._generateString(20).strip() for i in range(numUsers)]
+        discussionTitles = [self._generateString(20).strip() for i in range(numUsers)]
+        discussionTexts = [self._generateString(200).strip() for i in range(numUsers)]
+        
+        for i in range(numUsers):
+            thisUser = registration.create_and_activate_a_user(self, postal='95060', name=usernames[i])
+            thisWorkshop = workshop.create_new_workshop(self, thisUser, title=workshopTitles[i])
+            
+            objType = 'resource'
+            resource = nounAction.addNounToWorkshop(
+                self, 
+                objType, 
+                thisWorkshop, 
+                title=resourceTitles[i],
+                text=resourceTexts[i]
+            )
+            
+            objType = 'idea'
+            resource = nounAction.addNounToWorkshop(
+                self, 
+                objType, 
+                thisWorkshop, 
+                title=ideaTitles[i],
+                text=ideaTexts[i]
+            )
+            
+            objType = 'conversation'
+            resource = nounAction.addNounToWorkshop(
+                self, 
+                objType, 
+                thisWorkshop, 
+                title=discussionTitles[i],
+                text=discussionTexts[i]
+            )
+            
+            authLib.logout(self)
+        return {'usernames' : usernames,
+                'workshopTitles': workshopTitles,
+                'resourceTitles': resourceTitles,
+                'resourceTexts': resourceTexts,
+                'ideaTitles': ideaTitles,
+                'discussionTitles': discussionTitles,
+                'discussionTexts': discussionTexts}
     
     def _generateString(self, length, spacing = .1):
         """
@@ -57,7 +128,7 @@ class TestSearchController(TestController):
         for i in range(length):
             space = random.uniform(0, 1)
             if space > spacing:
-                index = random.randint(0, length - 1)
+                index = random.randint(0, len(source) - 1)
                 str += source[index]
             else:
                 str += ' '
