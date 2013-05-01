2aaec1c777fb14d4fe7de28f9443b19e4091b4c9
cneklason <cneklason@localhost>
2013-05-01 11:01:47 -0700

    Displays result messages as bootstrap alerts now.

--- pylowiki/public/js/ng/profile_edit.js
+++ pylowiki/public/js/ng/profile_edit.js
@@ -6,8 +6,13 @@ function ProfileEditController($scope, $http) {
         var submitURL = location.pathname + "/info/edit/handler"
         var thisForm = {member_name:$scope.fullName, email:$scope.email, postalCode:$scope.postalCode, greetingMsg:$scope.greetingMsg, websiteLink:$scope.websiteLink, websiteDesc:$scope.websiteDesc};
         $http.post(submitURL, thisForm).success(function(data){
+            var alertClass = 'alert-success';
+            if(data.statusCode == '1') {
+                alertClass = 'alert-error';   
+            }
+            var alertHTML = '<div class="alert ' + alertClass + '"><button type="button" class="close" data-dismiss="alert">&times;</button>' + data.result + '</div>'
             document.getElementById("submitResult").innerText = document.getElementById("submitResult").textContent = '';
-            document.getElementById("submitResult").innerText = document.getElementById("submitResult").textContent = data.result;
+            document.getElementById("submitResult").innerHTML = alertHTML;
             if(data.statusCode == '2'){
                 location.hash = 'tab-edit'
                 location.pathname = data.returnURL;
