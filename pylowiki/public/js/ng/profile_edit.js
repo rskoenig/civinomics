1c8d25a9393c5ae81be9f3c899fa1269cdd8b306
manu <manu@civinomics.com>
2013-05-10 10:43:50 -0700

    enhanced look of workshop_create, cleaned up workshop_payment

---
+++ pylowiki/public/js/ng/profile_edit.js
@@ -0,0 +1,42 @@
+function ProfileEditController($scope, $http) {
+    $scope.fullNameRegex = /^([A-Za-z0-9-_\s])+$/;
+    $scope.postalCodeRegex = /^\d*$/;
+    
+    $scope.submitProfileEdit = function(){
+        var submitURL = location.pathname + "/info/edit/handler"
+        var thisForm = {member_name:$scope.fullName, email:$scope.email, postalCode:$scope.postalCode, greetingMsg:$scope.greetingMsg, websiteLink:$scope.websiteLink, websiteDesc:$scope.websiteDesc};
+        $http.post(submitURL, thisForm).success(function(data){
+            var alertClass = 'alert-success';
+            if(data.statusCode == '1') {
+                alertClass = 'alert-error';   
+            }
+            var alertHTML = '<div class="alert ' + alertClass + '"><button type="button" class="close" data-dismiss="alert">&times;</button>' + data.result + '</div>'
+            document.getElementById("submitResult").innerText = document.getElementById("submitResult").textContent = '';
+            document.getElementById("submitResult").innerHTML = alertHTML;
+            window.history.replaceState('Object', 'Title', data.returnURL);
+            $scope.dashboardFullName = $scope.fullName;
+            $scope.dashboardGreetingMsg = $scope.greetingMsg;
+            $scope.dashboardWebsiteLink = $scope.websiteLink;
+            $scope.dashboardWebsiteDesc = $scope.websiteDesc;
+            if(data.statusCode == '2') {
+                $scope.updateGeoLinks();
+            }
+      });
+    }
+    $scope.updateGeoLinks = function(){
+        var submitURL = "/geo/cityStateCountryLink/" + $scope.postalCode
+        $http.get(submitURL).success(function(data){
+            if(data.statusCode == '0'){
+                $scope.cityTitle = data.cityTitle;
+                $scope.cityURL = data.cityURL;
+                $scope.stateTitle = data.stateTitle;
+                $scope.stateURL = data.stateURL;
+                $scope.countryTitle = data.countryTitle;
+                $scope.countryURL = data.countryURL;
+            }
+      });
+        
+        
+    }
+};
+
---
+++ pylowiki/templates/lib/derived/6_workshop_create.mako
@@ -0,0 +1,36 @@
+<%!
+    import logging
+    log = logging.getLogger(__name__)
+%>
+
+<%def name="createWorkshop()">
+    <div class="row-fluid">
+        <form id="CreateWorkshop" action = "/workshop/create/handler" class="form-vertical" method = "post">
+            <div class="span6">
+                <ul class="well orange pricing-table">
+                    <li class="title"><h3>Free</h3><img style="height: 48px" src="/images/glyphicons_pro/glyphicons/png/glyphicons_024_parents@2x.png" alt="couple icon"></li>
+                    <li class="price">$0 per month</li>
+                    <li class="description"> Totally free! </li>
+                    <li class="bullet-item"> Private </li>
+                    <li class="bullet-item"> Limited to 20 people </li>
+                    <li class="cta-button"> 
+                        <button type="submit" name="createPersonal" class="btn btn-large btn-civ">Go Free</button>
+                    </li>
+                </ul>
+            </div> <!-- /.span6 -->
+            <div class="span6">
+                <ul class="well purple pricing-table ">
+                    <li class="title"><h3>Pro</h3><img src="/images/glyphicons_pro/glyphicons/png/glyphicons_340_globe@2x.png" alt="globe icon"></li>
+                    <li class="price">$15 per month</li>
+                    <li class="description"> Best for community builders </li>
+                    <li class="bullet-item"> Public or private</li>
+                    <li class="bullet-item"> Unlimited private members </li>
+                    <li class="cta-button"> 
+                        <button type="submit" name="createProfessional" class="btn btn-large btn-civ">Go Pro</button>
+                    </li>
+                </ul>
+            </div> <!-- /.span6 -->
+        </form>
+    </div>
+</%def>
+
