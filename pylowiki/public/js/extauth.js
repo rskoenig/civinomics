053f3bbfe3966f83cd3e60d146e91538d7238a8a
Todd Anderson <todd@civinomics.com>
2013-07-10 13:40:47 -0700

    activation email bug fixed

---
+++ pylowiki/public/js/extauth.js
@@ -0,0 +1,21 @@
+function fbCheckAccount(response, authResponse, smallPic, bigPic){
+    console.log('in /public/js/extauth.js fbCheckAccount: ' + response.name);
+    //var newstring = 'email in extauth: ' + email;
+    //return newstring;
+    var encodedSmall = encodeURIComponent(smallPic)
+    encodedSmall = encodedSmall.replace(/\%/g, ",")
+    var encodedBig = encodeURIComponent(bigPic)
+    encodedBig = encodedBig.replace(/\%/g, ",")
+    var checkURL = "/extauth/fbEmail/" + response.name + "&" + response.email + "&" + authResponse.accessToken + "&" + authResponse.expiresIn + "&" + authResponse.signedRequest + "&" + authResponse.userID + "&" + encodedSmall + "&" + encodedBig 
+    
+    var checkResult = $.ajax({
+        type : 'POST',
+        async : false,
+        url : checkURL
+    }).responseText;
+    //console.log('cr ' + checkResult)
+    return checkResult;
+    //var gobj = jQuery.parseJSON(checkResult);
+    //return gobj.result
+    //document.getElementById("postalCodeResult").innerText = document.getElementById("postalCodeResult").textContent = gobj.result;
+}
