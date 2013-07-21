0d15a0a45728a58dd6fabd25c37c8c94c2d8d62f
cneklason <cneklason@localhost>
2013-07-20 19:43:02 -0700

    Better fix for avatar glitch.

---
+++ pylowiki/public/js/vendor/blueimp/app.js
@@ -0,0 +1,101 @@
+/*
+ * jQuery File Upload Plugin Angular JS Example 1.0
+ * https://github.com/blueimp/jQuery-File-Upload
+ *
+ * Copyright 2013, Sebastian Tschan
+ * https://blueimp.net
+ *
+ * Licensed under the MIT license:
+ * http://www.opensource.org/licenses/MIT
+ */
+
+/*global window, angular */
+
+(function () {
+    'use strict';
+
+    var isOnGitHub = window.location.hostname === 'blueimp.github.com' ||
+            window.location.hostname === 'blueimp.github.io';
+        //url = isOnGitHub ? '//jquery-file-upload.appspot.com/' : '/picture/upload/handler';
+        
+
+    angular.module('civ', [
+        'blueimp.fileupload'
+    ])
+        .config([
+            '$httpProvider', 'fileUploadProvider',
+            function ($httpProvider, fileUploadProvider) {
+                if (isOnGitHub) {
+                    // Demo settings:
+                    delete $httpProvider.defaults.headers.common['X-Requested-With'];
+                    angular.extend(fileUploadProvider.defaults, {
+                        disableImageResize: false,
+                        maxFileSize: 5000000,
+                        acceptFileTypes: /(\.|\/)(gif|jpe?g|png)$/i,
+                    });
+                }
+                else {
+                    angular.extend(fileUploadProvider.defaults, {
+                        disableImageResize: true,
+                        maxFileSize: 1000000, // 1MB
+                        acceptFileTypes: /(\.|\/)(gif|jpe?g|png)$/i,
+                        maxNumberOfFiles: 1,
+                        previewMaxWidth: 400,
+                        imageMaxWidth: 400,
+                    });
+                }
+            }
+        ])
+/*
+        .controller('DemoFileUploadController', [
+            '$scope', '$http',
+            function ($scope, $http) {
+                if (!isOnGitHub) {
+                    $scope.loadingFiles = true;
+                    $scope.options = {
+                        //url: '/profile/' + $scope.code + '/' + $scope.url + url
+                        url: url
+                    };
+                    $http.get($scope.options.url)
+                        .then(
+                            function (response) {
+                                $scope.loadingFiles = false;
+                                $scope.queue = response.data.files;
+                            },
+                            function () {
+                                $scope.loadingFiles = false;
+                            }
+                        );
+                }
+            }
+        ])
+*/
+        .controller('FileDestroyController', [
+            '$scope', '$http',
+            function ($scope, $http) {
+                var file = $scope.file,
+                    state;
+                if (file.url) {
+                    file.$state = function () {
+                        return state;
+                    };
+                    file.$destroy = function () {
+                        state = 'pending';
+                        return $http({
+                            url: file.delete_url,
+                            method: file.delete_type
+                        }).then(
+                            function () {
+                                state = 'resolved';
+                                $scope.clear(file);
+                            },
+                            function () {
+                                state = 'rejected';
+                            }
+                        );
+                    };
+                }
+            }
+        ]);
+
+}());
