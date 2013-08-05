dea61164ca52f3c7d6e18125ccac3433c50422fa
manu <manu@civinomics.com>
2013-08-05 13:58:52 -0700

    fixed walking score bug

--- pylowiki/public/js/yesNo.js
+++ pylowiki/public/js/yesNo.js
@@ -23,6 +23,7 @@ $(document).ready(function()
             $(this).children('img').attr("src", downURL);
          
             // having already placed a noVote, we will bring the score back up one place
+            // we will also reduce the total vote tally by 1 since the user has retracted their vote
             // (assuming there has not previously been a vote on this comment by this person)   
             $(this).attr("class", "noVote");
             var noScore = ($(this).children(".noScore").html()*1);
@@ -40,18 +41,20 @@ $(document).ready(function()
 
             $(this).attr("class", "noVote voted");
             if ($(this).siblings('.yesVote').hasClass('voted')) {
-                // replacing a yesVote with a noVote will bring the score down two places
+                // replacing a yesVote with a noVote will bring the tally/score of no votes up 1 and the tally/score of yes votes down 1
+                // total votes remains unchanged
                 // (assuming there has not previously been a vote on this comment by this person)
                 $(this).siblings('.yesVote').attr("class", "yesVote");
                 var noScore = ($(this).children(".noScore").html()*1);
                 noScore = noScore + 1;
                 $(this).children(".noScore").html(noScore); 
 
-                var yesScore = ($(this).children(".yesScore").html()*1);
+                var yesScore = ($(this).siblings(".yesVote").children(".yesScore").html()*1);
                 yesScore = yesScore - 1;
-                $(this).children(".yesScore").html(yesScore);    
+                $(this).siblings(".yesVote").children(".yesScore").html(yesScore);    
             } else {
-                 // a noVote from a neutral vote will bring the score down one place
+                 // a noVote from a neutral vote will add to the no tally/score
+                 // the total votes goes up by 1
                  // (assuming there has not previously been a vote on this comment by this person)
                 var noScore = ($(this).children(".noScore").html()*1);
                 noScore = noScore + 1;
@@ -72,7 +75,8 @@ $(document).ready(function()
         {
             $(this).children('img').attr("src", upURL);
 
-            // having already placed an yesVote, we will bring the score back down one place
+            // having already placed a yesVote, we will bring the score back down one place
+            // the user removes their vote so the vote count goes down
             // (assuming there has not previously been a vote on this comment by this person)
             $(this).attr("class", "yesVote");
             var yesScore = ($(this).children(".yesScore").html()*1);
@@ -90,18 +94,19 @@ $(document).ready(function()
 
             $(this).attr("class", "yesVote voted");
             if ($(this).siblings('.noVote').hasClass('voted')) {
-                // replacing a noVote with an yesVote will bring the score up two places
+                // replacing a noVote with a yesVote will bring the yes score up 1 and the no score down 1
                 // (assuming there has not previously been a vote on this comment by this person)
                 $(this).siblings('.noVote').attr("class", "noVote");
-                var noScore = ($(this).children(".noScore").html()*1);
+                var noScore = ($(this).siblings(".noVote").children(".noScore").html()*1);
                 noScore = noScore - 1;
-                $(this).children(".noScore").html(noScore); 
+                $(this).siblings(".noVote").children(".noScore").html(noScore); 
 
                 var yesScore = ($(this).children(".yesScore").html()*1);
                 yesScore = yesScore + 1;
                 $(this).children(".yesScore").html(yesScore);    
             } else {
-                 // an yesVote from a neutral vote will bring the score up one place
+                 // a yesVote from a neutral vote will bring the score up 1 place
+                 // it will also add to the total vote tally
                  // (assuming there has not previously been a vote on this comment by this person)
                 var yesScore = ($(this).children(".yesScore").html()*1);
                 yesScore = yesScore + 1;
