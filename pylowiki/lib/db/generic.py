9c082273627d0a69257aa31d1d207c8880e5a43d
Chris Neklason <chris@cruzio.com>
2013-08-15 14:28:49 -0700

    Merge development into default

---
+++ pylowiki/lib/db/generic.py
@@ -0,0 +1,131 @@
+import logging
+log = logging.getLogger(__name__)
+
+from pylowiki.model import Thing, Data, meta
+import sqlalchemy as sa
+from dbHelpers import with_characteristic as wc, commit
+from hashlib import md5
+
+def linkChildToParent(child, parent):
+    # Defines a standard for object linking.
+    # All objects that get accessed through a URL need the following two fields:
+    # 
+    # urlCode: obtained from toBase62() in pylowiki/lib/utils.py
+    # url: obtained from urlify() in pylowiki/lib/utils.py
+    # 
+    # Because the urlCode field is unique, this is what we use to link objects.
+    try:
+        code = parent['urlCode']
+    except Exception as e:
+        log.error("linkChildToParent(): parent object of type %s and id %s missing 'urlCode' field." %(parent.objType, parent.id))
+        return False
+    
+    key = '%s%s' %(parent.objType, 'Code')
+    if key in child:
+        # Overwrite, give warning
+        log.warning("linkChildToParent(): parent object link already exists in child.")
+    if 'workshop_category_tags' in parent:
+        child['workshop_category_tags'] = parent['workshop_category_tags']
+    if 'workshop_public_scope' in parent:
+        child['workshop_public_scope'] = parent['workshop_public_scope']
+    if 'workshop_searchable' in parent:
+        child['workshop_searchable'] = parent['workshop_searchable']
+        
+    child[key] = code
+    return child
+    
+def getThing(code, keys = None, values = None):
+    # Having some trouble doing this through map/reduce...for loop seems to work.  In looking at the debug
+    # output, it looks like the difference between SQLAlchemy making multiple sessions and a single session.
+    try:
+        q = meta.Session.query(Thing)\
+            .filter(Thing.data.any(wc('urlCode', code)))
+        if keys is None:
+            return q.one()
+        if type(keys) != type([]):
+            keys = [keys]
+            values = [values]
+        for i in range(len(keys)):
+            q = q.filter(Thing.data.any(wc(keys[i], values[i])))
+        return q.one()
+    except Exception as e:
+        return False
+        
+def getThingByID(thingID):
+    try:
+        return meta.Session.query(Thing)\
+            .filter_by(id = thingID)\
+            .one()
+    except:
+        return False
+        
+# mostly for linking listeners and pmembers to users
+def getThingsByEmail(email):
+    try:
+        q = meta.Session.query(Thing)\
+            .filter(Thing.data.any(wc('email', email)))
+        return q.all()
+    except Exception as e:
+        return False
+
+def addedItemAs(thing, privs, role = None):
+    """
+        thing       ->  A Thing object
+        privs       ->  The c.privs dict that sets permissions within a workshop
+        role        ->  (Optional) The preferred role to use, in string format.
+        
+        This sets the addedAs attribute for a given Thing.  For example, if someone is
+        posting a comment as a facilitator, then that comment gets the 'addedAs' attribute
+        set to 'facilitator'.
+        
+        If the attribute already exists, it overwrites.  If the attribute does not exist, it creates.
+    """
+    if role is not None:
+        thing['addedAs'] = role
+    else:
+        if privs['admin']:
+            thing['addedAs'] = 'admin'
+        elif privs['facilitator']:
+            thing['addedAs'] = 'facilitator'
+        elif privs['listener']:
+            thing['addedAs'] = 'listener'
+        else:
+            thing['addedAs'] = 'user'
+    return thing
+    
+def userImageSource(user, **kwargs):
+        # Assumes 'user' is a Thing.
+        # Defaults to a gravatar source
+        # kwargs:   forceSource:   Instead of returning a source based on the user-set preference in the profile editor,
+        #                          we return a source based on the value given here (civ/gravatar)
+        source = 'http://www.gravatar.com/avatar/%s?r=pg&d=identicon' % md5(user['email']).hexdigest()
+        large = False
+        gravatar = True
+
+        if 'className' in kwargs:
+            if 'avatar-large' in kwargs['className']:
+                large = True
+        if 'forceSource' in kwargs:
+            if kwargs['forceSource'] == 'civ':
+                gravatar = False
+                if 'directoryNum_avatar' in user.keys() and 'pictureHash_avatar' in user.keys():
+                    source = '/images/avatar/%s/avatar/%s.png' %(user['directoryNum_avatar'], user['pictureHash_avatar'])
+                else:
+                    source = '/images/glyphicons_pro/glyphicons/png/glyphicons_003_user.png'
+        elif 'extSource' in user.keys():
+            if 'facebookSource' in user.keys():
+                if user['facebookSource'] == u'1':
+                    gravatar = False
+                    # NOTE - when to provide large or small link?
+                    if large:
+                        source = user['facebookProfileBig']
+                    else:
+                        source = user['facebookProfileSmall']
+        else:
+            if 'avatarSource' in user.keys():
+                if user['avatarSource'] == 'civ':
+                    gravatar = False
+                    source = '/images/avatar/%s/avatar/%s.png' %(user['directoryNum_avatar'], user['pictureHash_avatar'])
+        if large and gravatar:
+            source += '&s=200'
+        return source
---
+++ pylowiki/templates/derived/list_owned_surveys/helpers.mako
@@ -0,0 +1,110 @@
+<%!
+    from pylowiki.lib.db.surveySlide import getSurveySlideByID
+    from pylowiki.lib.db.surveyAnswer import getAllAnswersForSurvey
+    from pylowiki.lib.db.user import getUserByID
+    
+    import logging
+    log = logging.getLogger(__name__)
+%>
+
+<%def name="showSurvey(survey, surveyNumber)">
+    <div class="accordion-heading">
+        <a href="#collapse${surveyNumber}" data-parent="#accordion" data-toggle="collapse" class="accordion-toggle">
+            % if int(survey['active']) == '0':
+                <span class="label label-info">Inactive</span>
+            % else:
+                <span class="label label-success">Active</span>
+            % endif
+            ${survey['title']}
+        </a>
+    </div>
+    <div class="accordion-body collapse" id="collapse${surveyNumber}">
+        <div class="accordion-inner">
+            <div class="btn-group">
+                <a href = "/survey/${survey['urlCode']}/${survey['url']}/edit" class="btn">Edit</a>
+                % if survey['hash'] == 'flash':
+                    <a href = "/survey/${survey['urlCode']}/${survey['url']}/upload" class="btn btn-warning">
+                        Upload
+                    </a>
+                % else:
+                    <a href = "/survey/${survey['urlCode']}/${survey['url']}/upload" class="btn">
+                        Upload
+                    </a>
+                % endif
+                % if survey['hash'] != 'flash':
+                    <%
+                        slides = map(int, survey['slides'].split(','))
+                        firstSlide = getSurveySlideByID(slides[0])
+                    %>
+                    <a href = "/survey/${survey['urlCode']}/${survey['url']}/page/${firstSlide['hash']}" class="btn">
+                        View
+                    </a>
+                % endif
+                <% results = getAllAnswersForSurvey(survey) %>
+                % if len(results) > 0:
+                    <a href="/viewResults/${survey['urlCode']}/${survey['url']}" class="btn">Results</a>
+                % endif
+            </div>
+            <br />
+            <table class="table table-striped">
+                <tbody>
+                    <tr>
+                        <td>Name: </td>
+                        <td>${survey['title']}</td>
+                    </tr>
+                    <tr>
+                        <td>Description: </td>
+                        <td>${survey['description']}</td>
+                    </tr>
+                    <tr>
+                        <td>Date created: </td>
+                        <td>${survey.date} PST</td>
+                    </tr>
+                    <tr>
+                        <td>Public or Private: </td>
+                        <td>${survey['publicOrPrivate']}</td>
+                    </tr>
+                    <tr>
+                        <td>Survey file: </td>
+                        % if survey['hash'] == 'flash':
+                            <td>No file uploaded!</td>
+                        % else:
+                            <td>
+                                <a href = "/surveys/${survey['surveyType']}/${survey['directoryNum']}/${survey['hash']}/${survey['origFileName']}">
+                                    ${survey['origFileName']}
+                                </a>
+                            </td>
+                        % endif
+                    </tr>
+                    <%
+                        ids = map(int, survey['facilitators'].split(','))
+                        if len(ids) > 1:
+                            ids = ids[1:]
+                        
+                        facilitators = []
+                        facilitators.append(getUserByID(survey.owner))
+                        if len(ids) > 0:
+                            for id in ids:
+                                u = getUserByID(id)
+                                if u:
+                                    facilitators.append(u)
+                    %>
+                    % for facilitator in facilitators:
+                        <tr>
+                            <td>Facilitator: </td>
+                            <td>${facilitator['name']}</td>
+                        </tr>
+                    % endfor
+                    <tr>
+                        <td>Postal codes:</td>
+                        <td>${survey['publicPostalList']}</td>
+                    </tr>
+                    <tr>
+                        <td>Estimated Time:</td>
+                        <td>${survey['estimatedTime']} minutes</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</%def>
