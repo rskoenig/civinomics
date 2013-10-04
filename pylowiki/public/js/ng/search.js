38b770c5005e6dc132482cc011bce11e8447813b
Chris Neklason <chris@cruzio.com>
2013-10-04 11:01:43 -0700

    Merge development into release

--- pylowiki/public/js/ng/search.js
+++ pylowiki/public/js/ng/search.js
@@ -319,8 +319,12 @@ app.controller('SearchCtrl', function($scope, $http){
 
 app.filter('startFrom', function() {
     return function(input, start) {
-        start = +start; //parse to int
-        return input.slice(start);
+        if(start) {
+            start = +start; //parse to int
+            return input.slice(start);
+        } else {
+            return input;
+        }
     }
 });
 
