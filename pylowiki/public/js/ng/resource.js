c6e264eeba8cf49243c92f8d7fea3224005d9c76
cneklason <cneklason@localhost>
2013-10-08 19:23:48 -0700

    Fixed search where there were spaces involved, cleaned up some log output.

---
+++ pylowiki/public/js/ng/resource.js
@@ -0,0 +1,44 @@
+
+function resourceController($scope, $http, $location, $timeout) {
+    $scope.addResourceTitleShow = false;
+    $scope.addResourceURLShow = false;
+    
+    $scope.submitResourceForm = function(addResourceForm) {
+
+
+        if(addResourceForm.title.$invalid) {
+            $scope.addResourceTitleShow = true;
+            $scope.addResourceTitleResponse =  "Title is required.";
+        } else {
+            $scope.addResourceTitleShow = false;
+            $scope.addResourceTitleResponse =  "";  
+        }
+        if(addResourceForm.link.$invalid) {
+            $scope.addResourceURLShow = true;
+            $scope.addResourceURLResponse =  "A valid link URL is required.";
+        } else {
+            $scope.addResourceURLShow = false;
+            $scope.addResourceURLResponse =  "";
+        }
+        if(addResourceForm.$valid) {
+            $scope.addResourceShow = true;
+            $scope.addResourceResponse = "Submitting resource...";
+            var addURL = '/workshop/' + $scope.workshopCode + '/' + $scope.workshopURL + '/add/resource/handler';
+            var postData = {'title':$scope.title, 'link': $scope.link, 'text': $scope.text};
+            $http.post(addURL, postData).success(function(data){
+                if(data.state == 'Error'){
+                    $scope.addResourceShow = true;
+                    $scope.addResourceResponse = data.errorMessage;
+                } else {
+                    var resourceCode = data.resourceCode;
+                    var resourceURL = data.resourceURL;
+
+                    var newResourceURL = '/workshop/' + $scope.workshopCode + '/' + $scope.workshopURL + '/resource/' + resourceCode + '/' + resourceURL;
+                    window.location = newResourceURL;
+                }
+            });
+        }
+        
+    };
+
+}
