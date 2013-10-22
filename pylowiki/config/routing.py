97ef89f1ded2f961eeaa10e50a5018be057caade
manu <manu@civinomics.com>
2013-10-22 14:14:31 -0700

    fixed bug caused by 1st draft of redirect that prevented logout

--- pylowiki/config/routing.py
+++ pylowiki/config/routing.py
@@ -290,7 +290,7 @@ def make_map():
     map.connect('/workshop/{workshopCode}/{workshopURL}/login{end:/?}', controller = 'login', action = 'loginDisplay', workshopCode = '{workshopCode}', workshopURL = '{workshopURL}', thing = 'None', thingCode = 'None', thingURL = 'None')
     map.connect('/workshop/{workshopCode}/{workshopURL}/login/{thing}{end:/?}', controller = 'login', action = 'loginDisplay', workshopCode = '{workshopCode}', workshopURL = '{workshopURL}', thing = '{thing}', thingCode = 'None', thingURL = 'None')
     map.connect('/workshop/{workshopCode}/{workshopURL}/clogin/{thing}/{thingCode}/{thingURL}{end:/?}', controller = 'login', action = 'loginDisplay', workshopCode = '{workshopCode}', workshopURL = '{workshopURL}', thing = '{thing}', thingCode = '{thingCode}', thingURL = '{thingURL}')
-    map.connect('/login/{page}{end:/?}', controller = 'login', action = 'loginRedirects', page = '{page}')
+    map.connect('/{page}/login{end:/?}', controller = 'login', action = 'loginRedirects', page = '{page}')
     map.connect('/{loginHandler:loginHandler/?}', controller = 'login', action = 'loginHandler')
     map.connect('/{signup:signup/?}', controller = 'register', action = 'signupDisplay')
     map.connect('/{signupNoExtAuth:signupNoExtAuth/?}', controller = 'register', action = 'signupNoExtAuthDisplay')
