22bb78bbfa3f4841af862760ff436d9fd446a12a
Chris Neklason <chris@cruzio.com>
2013-10-24 11:44:12 -0700

    Closed branch iphone_app_final

---
+++ pylowiki/public/js/ng/signup_login.js
@@ -0,0 +1,9 @@
+function signupController($scope) {
+    $scope.postalCodeRegex = /^\d{5}$/;
+    $scope.fullNameRegex = /^([A-Za-z0-9-_\s])+$/;
+    $scope.email = '';
+    
+    $scope.clearEmail = function() {
+        $scope.email = '';
+    };
+}
