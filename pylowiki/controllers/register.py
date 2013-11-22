8f764b41a7aeefdece33a584403542eabac5edf2
Todd Anderson <todd@civinomics.com>
2013-11-22 12:38:41 -0800

    forgot to include the utils library

--- pylowiki/controllers/register.py
+++ pylowiki/controllers/register.py
@@ -5,17 +5,18 @@ from pylons import request, response, session, tmpl_context as c
 from pylons.controllers.util import abort, redirect
 from pylons import config
 
-from pylowiki.lib.base import BaseController, render
-import pylowiki.lib.helpers as h
+from pylowiki.lib.base      import BaseController, render
+import pylowiki.lib.helpers     as h
+import pylowiki.lib.utils       as utils
 
-from pylowiki.lib.db.user import User, getUserByEmail, getUserByFacebookAuthId, getActiveUsers, getUserByTwitterId
-from pylowiki.lib.db.pmember import getPrivateMemberByCode
-from pylowiki.lib.db.workshop import getWorkshopByCode, setWorkshopPrivs
-from pylowiki.lib.db.geoInfo import getPostalInfo
-from pylowiki.lib.db.dbHelpers import commit
-import pylowiki.lib.db.mainImage    as mainImageLib
-from pylowiki.lib.db.revision import Revision
-import pylowiki.lib.mail            as mailLib
+from pylowiki.lib.db.user         import User, getUserByEmail, getUserByFacebookAuthId, getActiveUsers, getUserByTwitterId
+from pylowiki.lib.db.pmember      import getPrivateMemberByCode
+from pylowiki.lib.db.workshop     import getWorkshopByCode, setWorkshopPrivs
+from pylowiki.lib.db.geoInfo      import getPostalInfo
+from pylowiki.lib.db.dbHelpers    import commit
+import pylowiki.lib.db.mainImage  as mainImageLib
+from pylowiki.lib.db.revision     import Revision
+import pylowiki.lib.mail          as mailLib
 import re
 import simplejson as json
 
