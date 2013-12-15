e0cefc521cd199d9ffc76310e4ee47392fff2f94
manu <manu@civinomics.com>
2013-12-14 17:21:07 -0800

    created branch iSplash; centered and narrowed welcome section

---
+++ pylowiki/controllers/discussion.py
@@ -0,0 +1,172 @@
+import logging, pickle
+
+from pylons import config, request, response, session, tmpl_context as c
+from pylons.controllers.util import abort, redirect_to, redirect
+import webhelpers.paginate as paginate
+from pylowiki.lib.base import BaseController, render
+
+from pylowiki.lib.db.dbHelpers import commit
+import pylowiki.lib.utils as utils
+import pylowiki.lib.db.event        as eventLib
+import pylowiki.lib.db.workshop     as workshopLib
+import pylowiki.lib.db.discussion   as discussionLib
+import pylowiki.lib.db.comment      as commentLib
+import pylowiki.lib.db.user         as userLib
+import pylowiki.lib.db.facilitator  as facilitatorLib
+import pylowiki.lib.db.flag         as flagLib
+import pylowiki.lib.db.rating       as ratingLib
+import pylowiki.lib.db.revision     as revisionLib
+import pylowiki.lib.db.geoInfo      as geoInfoLib
+import pylowiki.lib.db.mainImage    as mainImageLib
+import pylowiki.lib.db.dbHelpers    as dbHelpers
+import pylowiki.lib.alerts          as  alertsLib
+
+from pylowiki.lib.sort import sortBinaryByTopPop, sortContByAvgTop
+
+import pylowiki.lib.helpers as h
+import simplejson as json
+
+log = logging.getLogger(__name__)
+
+class DiscussionController(BaseController):
+
+    def __before__(self, action, workshopCode = None):
+        publicOrPrivate = ['index', 'topic', 'thread']
+        
+        if workshopCode is None:
+            abort(404)
+        c.w = workshopLib.getWorkshopByCode(workshopCode)
+        if not c.w:
+            abort(404)
+            
+        c.mainImage = mainImageLib.getMainImage(c.w)
+        
+        # Demo workshop status
+        c.demo = workshopLib.isDemo(c.w)
+        
+        workshopLib.setWorkshopPrivs(c.w)
+        if c.w['public_private'] == 'public':
+            c.scope = geoInfoLib.getPublicScope(c.w)
+        if action in publicOrPrivate:
+            if c.w['public_private'] != 'public':
+                if not c.privs['guest'] and not c.privs['participant'] and not c.privs['facilitator'] and not c.privs['admin']:
+                    abort(404)
+        if 'user' in session:
+            utils.isWatching(c.authuser, c.w)
+
+    def index(self, workshopCode, workshopURL):
+        c.title = c.w['title']
+        #get the scope to display jurisidction flag
+        if c.w['public_private'] == 'public':
+            c.scope = workshopLib.getPublicScope(c.w)
+        c.discussions = discussionLib.getDiscussionsForWorkshop(workshopCode)
+        if not c.discussions:
+            c.discussions = []
+        else:
+            c.discussions = sortBinaryByTopPop(c.discussions)
+        disabled = discussionLib.getDiscussionsForWorkshop(workshopCode, disabled = '1')
+        if disabled:
+            c.discussions = c.discussions + disabled
+        c.listingType = 'discussion'
+        return render('/derived/6_detailed_listing.bootstrap')
+
+    def topic(self, workshopCode, workshopURL, discussionCode, discussionURL):
+        #get the scope to display jurisidction flag
+        if c.w['public_private'] == 'public':
+            c.scope = workshopLib.getPublicScope(c.w)
+        # these values are needed for facebook sharing
+        c.facebookAppId = config['facebook.appid']
+        c.channelUrl = config['facebook.channelUrl']
+        c.baseUrl = config['site_base_url']
+        # for creating a link, we need to make sure baseUrl doesn't have an '/' on the end
+        if c.baseUrl[-1:] == "/":
+            c.baseUrl = c.baseUrl[:-1]
+        c.requestUrl = request.url
+        c.thingCode = discussionCode
+        # standard thumbnail image for facebook shares
+        if c.mainImage['pictureHash'] == 'supDawg':
+            c.backgroundImage = '/images/slide/slideshow/supDawg.slideshow'
+        elif 'format' in c.mainImage.keys():
+            c.backgroundImage = '/images/mainImage/%s/orig/%s.%s' %(c.mainImage['directoryNum'], c.mainImage['pictureHash'], c.mainImage['format'])
+        else:
+            c.backgroundImage = '/images/mainImage/%s/orig/%s.jpg' %(c.mainImage['directoryNum'], c.mainImage['pictureHash'])
+
+        c.thing = c.discussion = discussionLib.getDiscussion(discussionCode)
+        if not c.thing:
+            c.thing = revisionLib.getRevisionByCode(discussionCode)
+            if not c.thing:
+                abort(404)
+        # name/title for facebook sharing
+        c.name = c.thing['title']
+        if 'views' not in c.thing:
+            c.thing['views'] = u'0'
+            
+        views = int(c.thing['views']) + 1
+        c.thing['views'] = str(views)
+        dbHelpers.commit(c.thing)
+
+        c.flags = flagLib.getFlags(c.thing)
+        c.events = eventLib.getParentEvents(c.thing)
+        c.title = c.w['title']
+        c.revisions = revisionLib.getRevisionsForThing(c.thing)
+        c.listingType = 'discussion'
+        
+        if 'comment' in request.params:
+            c.rootComment = commentLib.getCommentByCode(request.params['comment'])
+            if not c.rootComment:
+                abort(404)
+        return render('/derived/6_item_in_listing.bootstrap')
+
+    def thread(self, workshopCode, workshopURL, discussionCode, discussionURL, commentCode):
+        c.rootComment = commentLib.getCommentByCode(commentCode)
+        c.discussion = discussionLib.getDiscussionByID(c.rootComment['discussion_id'])
+        c.title = c.w['title']
+        c.content = h.literal(h.reST2HTML(c.discussion['text']))
+        c.listingType = 'discussion'
+        return render('/derived/6_item_in_listing.bootstrap')
+
+    def addDiscussion(self, workshopCode, workshopURL):
+        if c.privs['participant'] or c.privs['admin'] or c.privs['facilitator']:
+            c.title = c.w['title']
+            #get the scope to display jurisidction flag
+            if c.w['public_private'] == 'public':
+                c.scope = workshopLib.getPublicScope(c.w)
+            c.listingType = 'discussion'
+            return render('/derived/6_add_to_listing.bootstrap')
+        elif c.privs['guest']:
+            c.listingType = 'discussion'
+            return render('/derived/6_guest_signup.bootstrap')
+        else:
+            return redirect('/workshop/%s/%s' % (c.w['urlCode'], c.w['url']))
+
+    @h.login_required
+    def addDiscussionHandler(self, workshopCode, workshopURL):
+        if not c.privs['participant'] and not c.privs['admin'] and not c.privs['facilitator']:
+            return redirect('/workshop/%s/%s' % (c.w['urlCode'], c.w['url']))
+       
+        if 'title' in request.params:
+            title = request.params['title']
+        else: 
+            title = False
+        if 'text' in request.params:
+            text = request.params['text']
+        else:
+            text = ''
+
+        if not title or title=='':
+            alert = {'type':'error'}
+            alert['title'] = 'Title Field Required'
+            alert['content'] = ''
+            session['alert'] = alert
+            session.save()
+            return redirect(session['return_to'])
+
+        else:
+            if len(title) > 120:
+                title = title[:120]
+            d = discussionLib.Discussion(owner = c.authuser, discType = 'general', attachedThing = c.w,\
+                title = title, text = text, workshop = c.w, privs = c.privs, role = None)
+            alertsLib.emailAlerts(d.d)
+            commit(c.w)
+        
+        return redirect(utils.thingURL(c.w, d.d))
