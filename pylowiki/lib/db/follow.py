baecaec7df4cef7144aa468f74dc0b556ab8d9b9
Todd Anderson <todd@civinomics.com>
2014-02-11 15:26:21 -0800

    work beginning on using angular in the messages page

---
+++ pylowiki/lib/db/follow.py
@@ -0,0 +1,151 @@
+#-*- coding: utf-8 -*-
+import logging
+
+from sqlalchemy import and_
+from pylons import session, tmpl_context as c
+from pylowiki.model import Thing, Data, meta
+import pylowiki.lib.db.generic as generic
+from dbHelpers import commit, with_characteristic as wc
+
+log = logging.getLogger(__name__)
+
+# Getters
+# Who is following the workshop
+def getWorkshopFollowers( workshop, disabled = '0'):
+    try:
+        return meta.Session.query(Thing)\
+            .filter_by(objType = 'follow')\
+            .filter(Thing.data.any(wc('disabled', disabled)))\
+            .filter(Thing.data.any(wc('workshopCode', workshop['urlCode']))).all()
+    except:
+        return False
+
+# Who is following the user
+def getUserFollowers( user, disabled = '0'):
+    try:
+        return meta.Session.query(Thing)\
+            .filter_by(objType = 'follow')\
+            .filter(Thing.data.any(wc('disabled', disabled)))\
+            .filter(Thing.data.any(wc('userCode', user['urlCode']))).all()
+    except:
+        return False
+
+# Which workshops is the user following
+def getWorkshopFollows( user, disabled = '0'):
+    try:
+        return meta.Session.query(Thing)\
+            .filter_by(objType = 'follow')\
+            .filter_by(owner = user.id)\
+            .filter(Thing.data.any(wc('disabled', disabled)))\
+            .filter(Thing.data.any(and_(Data.key == u'workshopCode'))).all()
+    except:
+        return False
+        
+# Which initiatives is the user following
+def getInitiativeFollows( user, disabled = '0'):
+    try:
+        return meta.Session.query(Thing)\
+            .filter_by(objType = 'follow')\
+            .filter_by(owner = user.id)\
+            .filter(Thing.data.any(wc('disabled', disabled)))\
+            .filter(Thing.data.any(and_(Data.key == u'initiativeCode'))).all()
+    except:
+        return False
+        
+# Which users is the user following
+def getUserFollows( user, disabled = '0'):
+    try:
+        return meta.Session.query(Thing)\
+            .filter_by(objType = 'follow')\
+            .filter_by(owner = user.id)\
+            .filter(Thing.data.any(wc('disabled', disabled)))\
+            .filter(Thing.data.any(and_(Data.key == u'userCode'))).all()
+    except:
+        return False
+
+# get the Follow object for this user and thing
+def getFollow( user, thing ):
+    try:
+        thingCode = '%sCode' % thing.objType
+        return meta.Session.query(Thing)\
+            .filter_by(objType = 'follow')\
+            .filter_by(owner = user.id)\
+            .filter(Thing.data.any(wc(thingCode, thing['urlCode']))).one()
+    except:
+        return False
+
+# is the user following the thing
+def isFollowing(user, thing):
+    try:
+        thingCode = '%sCode' % thing.objType
+        f = meta.Session.query(Thing)\
+            .filter_by(objType = 'follow')\
+            .filter_by(owner = user.id)\
+            .filter(Thing.data.any(wc('disabled', False)))\
+            .filter(Thing.data.any(wc(thingCode, thing['urlCode']))).all()
+        if f:
+            return True
+        else:
+            return False
+    except:
+        log.info("broken following query")
+        return False
+
+# Object creation/modification here
+def FollowOrUnfollow(user, thing, disabled = '0'):
+    # We link the follow object to the user and the thing being followed.  The follow object is set up as a child of both, and points to both.
+    # The link to the user is done by the 'owner' property (dot notation), and the link to the thing is done by the 'thingCode' attribute
+    # (dict notation).
+    try:
+        f = getFollow(user, thing)
+        if f:
+            # Ugly hack to reverse the bit when it's stored as a string
+            f['disabled'] = str(int(not int(f['disabled'])))
+        else:
+            f = Thing('follow', user.id)
+            generic.linkChildToParent(f, thing)
+            f['itemAlerts'] = '0'
+            f['digest'] = '0'
+            f['disabled'] = disabled
+        
+        if thing.objType == 'user': 
+            fKey = 'follower_counter'
+            if f['disabled'] == '0':
+                if fKey in thing:
+                    fValue = int(thing[fKey])
+                    fValue += 1
+                    thing[fKey] = str(fValue)
+                else:
+                    thing[fKey] = '1'
+            else:
+                if fKey in thing:
+                    fValue = int(thing[fKey])
+                    fValue -= 1
+                    thing[fKey] = str(fValue)
+                else:
+                    thing[fKey] = '0'
+            fKey = 'follow_counter'
+        elif thing.objType == 'workshop' or thing.objType == 'initiative':
+            fKey = 'bookmark_counter'
+        
+        if f['disabled'] == '0':
+            if fKey in user:
+                fValue = int(user[fKey])
+                fValue += 1
+                user[fKey] = str(fValue)
+            else:
+                user[fKey] = '1'
+        else:
+            if fKey in user:
+                fValue = int(user[fKey])
+                fValue -= 1
+                user[fKey] = str(fValue)
+            else:
+                user[fKey] = '0'
+
+        commit(user)
+        commit(f)
+        return True
+    except:
+        return False
+        
