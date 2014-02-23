8bd590e4adf440b340c5988ef430b7309a775d04
manu <manu@civinomics.com>
2014-02-23 00:18:32 -0800

    display of all activity object types ported to workshops

--- pylowiki/controllers/workshop.py
+++ pylowiki/controllers/workshop.py
@@ -1437,3 +1437,167 @@ class WorkshopController(BaseController):
         return json.dumps({'statusCode': 0, 'result': result, 'adopted' : adopted, 'numIdeas' : numIdeas})
 
 
+    def getWorkshopActivity(self, comments = 0, type = 'auto', offset = 0, max = 7):
+        # get recent activity and return it into json format
+        result = []
+
+        offset = int(offset)
+        commments = int(comments)
+
+        #recentActivity = activityLib.getRecentActivity(max)
+        workshopActivity = activityLib.getActivityForWorkshop(c.w['urlCode'])
+        
+        myRatings = {}
+        if 'ratings' in session:
+            myRatings = session['ratings']
+
+        for item in workshopActivity:
+            entry = {}
+            # item attributes
+            entry['title'] = item['title']
+            entry['objType'] = item.objType
+            if item.objType == 'discussion':
+                if item['discType'] == 'update':
+                    entry['objType'] = 'update'
+            entry['urlCode'] = item['urlCode']
+            entry['url'] = item['url']
+            entry['date'] = item.date.strftime('%Y-%m-%d at %H:%M:%S')
+            entry['fuzzyTime'] = fuzzyTime.timeSince(item.date)
+            if 'views' in item:
+                entry['views'] = str(item['views'])
+            else:
+                entry['views'] = '0'
+
+            # attributes that vary accross items
+            entry['text'] = '0'
+            if 'text' in item:
+                entry['text'] = item['text']
+            elif 'description' in item:
+                entry['text'] = item['description']
+            entry['html'] = m.html(entry['text'], render_flags=m.HTML_SKIP_HTML)
+            if 'link' in item:
+                entry['link'] = item['link']
+            else:
+                entry['link'] = '0'
+            if 'cost' in item:
+                entry['cost'] = item['cost']
+            else:
+                entry['cost'] = ''
+            entry['article'] = 'a'
+            if entry['objType'] == 'idea' or entry['objType'] == 'update' or entry['objType'] == 'initiative':
+                entry['article'] = 'an'
+
+            # href
+            # note: we should standardize the way object urls are constructed
+            if item.objType == 'photo':
+                entry['href'] = '/profile/' + item['userCode'] + '/' + item['user_url'] + "/photo/show/" + item['urlCode']
+            else:
+                entry['href'] = '/' + item.objType + '/' + item['urlCode'] + '/' + item['url']
+
+            if 'workshopCode' in item:
+                entry['parentHref'] = '/workshop/' + item['workshopCode'] + '/' + item['workshop_url']
+                entry['href'] = entry['parentHref'] + entry['href']
+            elif 'initiativeCode' in item:
+                entry['parentHref'] = '/initiative/' + item['initiativeCode'] + '/' + item['initiative_url']
+                if entry['objType'] == 'update':
+                    entry['href'] = entry['parentHref'] + '/updateShow/' + item['urlCode']
+                else:
+                    entry['href'] = entry['parentHref'] + entry['href']
+            
+            # modifications for children of workshops and initiatives
+            entry['parentTitle'] = ''
+            entry['parentObjType'] = ''
+            if 'workshopCode' in item:
+                entry['parentTitle'] = item['workshop_title']
+                entry['parentObjType'] = 'workshop'
+            elif 'initiativeCode' in item:
+                entry['parentTitle'] = item['initiative_title']
+                entry['parentObjType'] = 'initiative'
+
+            # photo
+            if 'directoryNum_photos' in item and 'pictureHash_photos' in item:
+                entry['mainPhoto'] = "/images/photos/%s/photo/%s.png"%(item['directoryNum_photos'], item['pictureHash_photos'])
+                entry['thumbnail'] = "/images/photos/%s/thumbnail/%s.png"%(item['directoryNum_photos'], item['pictureHash_photos'])
+            else:
+                entry['mainPhoto'] = '0'
+                entry['thumbnail'] = '0'
+
+            #tags
+            tags = []
+            tagList = []
+            if 'tags' in item:
+                tagList = item['tags'].split('|')
+            elif 'initiative_tags' in item:
+                tagList = item['initiative_tags'].split('|')
+            elif 'workshop_category_tags' in item:
+                tagList = item['workshop_category_tags'].split('|')
+            for tag in tagList:
+                if tag and tag != '':
+                    tags.append(tag)
+            entry['tags'] = tags
+
+            # scope attributes
+            if 'scope' in item:
+                entry['scope'] = item['scope']
+            elif 'initiative_scope' in item:
+                entry['scope'] = item['initiative_scope']
+            elif 'workshop_public_scope' in item:
+                entry['scope'] = item['workshop_public_scope']
+            else:
+                entry['scope'] = '0||united-states||0||0||0|0'
+            scopeInfo = utils.getPublicScope(entry['scope'])
+            entry['scopeName'] = scopeInfo['name']
+            entry['scopeLevel'] = scopeInfo['level']
+            entry['scopeHref'] = scopeInfo['href']
+            entry['flag'] = scopeInfo['flag']
+
+            # user rating
+            if entry['urlCode'] in myRatings:
+                entry['rated'] = myRatings[entry['urlCode']]
+                entry['vote'] = 'voted'
+            else:
+                entry['rated'] = 0
+                entry['vote'] = 'nvote'
+
+            # votes
+            entry['voteCount'] = int(item['ups']) + int(item['downs'])
+            entry['ups'] = int(item['ups'])
+            entry['downs'] = int(item['downs'])
+            entry['netVotes'] = int(item['ups']) - int(item['downs'])
+
+            # comments
+            discussion = discussionLib.getDiscussionForThing(item)
+            entry['discussion'] = discussion['urlCode']
+            entry['numComments'] = 0
+            if 'numComments' in item:
+                entry['numComments'] = item['numComments']
+
+            # author data
+            # CCN - need to find a way to optimize this lookup
+            author = userLib.getUserByID(item.owner)
+            entry['authorName'] = author['name']
+            entry['authorPhoto'] = utils._userImageSource(author)
+            entry['authorCode'] = author['urlCode']
+            entry['authorURL'] = author['url']
+            entry['authorHref'] = '/profile/' + author['urlCode'] + '/' + author['url']
+
+            entry['parentTitle'] = ''
+            entry['parentObjType'] = ''
+            entry['article'] = 'a'
+            if entry['objType'] == 'idea' or entry['objType'] == 'update' or entry['objType'] == 'initiative':
+                entry['article'] = 'an'
+
+            # modifications for children of workshops and initiatives
+            if 'workshopCode' in item:
+                entry['parentTitle'] = item['workshop_title']
+                entry['parentObjType'] = 'workshop'
+            elif 'initiativeCode' in item:
+                entry['parentTitle'] = item['initiative_title']
+                entry['parentObjType'] = 'initiative'
+
+            result.append(entry)
+
+        if len(result) == 0:
+            return json.dumps({'statusCode':1})
+        return json.dumps({'statusCode':0, 'result': result})
+
