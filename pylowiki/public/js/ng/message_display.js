60fe62af3e795a2394bc84bba087645d69546cb6
Todd Anderson <todd@civinomics.com>
2014-03-03 15:49:34 -0800

    set up a better initialization method for marking unread messages

--- pylowiki/public/js/ng/message_display.js
+++ pylowiki/public/js/ng/message_display.js
@@ -4,7 +4,7 @@ function messageDisplayCtrl($scope, $http) {
     $scope.classUnread = '';
 
     
-    $scope.updateReadStatus = function() {
+    $scope.updateReadStatus = function(initializing) {
         if ($scope.classUnread == 'warning unread-message') {
             $scope.classUnread = '';
             $.post('/message/' + $scope.messageCode + '/mark/read/').
@@ -15,14 +15,14 @@ function messageDisplayCtrl($scope, $http) {
                     //console.log(data);
                     $scope.classUnread = 'warning unread-message';
                 });
-        } else if ($scope.read == '0') {
+        } else if ($scope.read == '0' && initializing==true) {
             $scope.classUnread = 'warning unread-message';
         } else {
             $scope.classUnread = '';
         }
     }
 
-    $scope.updateReadStatus();
+    $scope.updateReadStatus(true);
 
     $scope.isRead= function(read) {
         if (read == '1') {
