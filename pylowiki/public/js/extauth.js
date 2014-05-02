c6de60047d3ff09812ac68483a1a40fc959cca45
Todd Anderson <todd@civinomics.com>
2014-05-02 16:59:22 -0700

    EETS WORKEENG

--- pylowiki/public/js/extauth.js
+++ pylowiki/public/js/extauth.js
@@ -1,17 +1,12 @@
 function fbCheckAccount(responseName, authResponse, smallPic, bigPic){
     // NOTE: response.email not working, can't retrieve email at this point
 
-    //console.log('in /public/js/extauth.js fbCheckAccount: ' + response.name);
-    //var newstring = 'email in extauth: ' + email;
-    //return newstring;
-    //console.log('hello')
-
     var encodedSmall = encodeURIComponent(smallPic)
     encodedSmall = encodedSmall.replace(/\%/g, ",")
     var encodedBig = encodeURIComponent(bigPic)
     encodedBig = encodedBig.replace(/\%/g, ",")
     var checkURL = "/extauth/fbEmail/" + responseName + "&" + 'emailNotAvailable' + "&" + authResponse.accessToken + "&" + authResponse.expiresIn + "&" + authResponse.signedRequest + "&" + authResponse.userID + "&" + encodedSmall + "&" + encodedBig 
-    console.log('urlcheck: '+checkURL)
+    //console.log('urlcheck: '+checkURL)
     var checkResult = $.ajax({
         type : 'POST',
         async : false,
@@ -19,9 +14,6 @@ function fbCheckAccount(responseName, authResponse, smallPic, bigPic){
     }).responseText;
     //console.log('cr ' + checkResult)
     return checkResult;
-    //var gobj = jQuery.parseJSON(checkResult);
-    //return gobj.result
-    //document.getElementById("postalCodeResult").innerText = document.getElementById("postalCodeResult").textContent = gobj.result;
 }
 
 function postShared(response, itemCode, itemURL, postId, userCode, workshopCode, shareType){
