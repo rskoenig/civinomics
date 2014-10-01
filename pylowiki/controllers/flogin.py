0bdf921fbeb357f335a48daf2caff0bb74f9a868
Chris Neklason <chris@cruzio.com>
2014-09-30 20:54:19 -0700

    Merge default into development

---
+++ pylowiki/controllers/flogin.py
@@ -0,0 +1,199 @@
+# -*- coding: utf-8 -*-
+import logging, time
+
+from pylons import request, response, session, tmpl_context as c, url
+from pylons.controllers.util import abort, redirect
+from pylons.controllers.login import logUserIn
+from pylons import config
+
+from urllib import urlencode
+
+from pylowiki.lib.base import BaseController, render
+
+import pylowiki.lib.helpers as h
+import pylowiki.lib.db.user as userLib
+import pylowiki.lib.mail as mailLib
+from pylowiki.lib.auth import login_required
+from pylowiki.lib.db.dbHelpers import commit
+
+import simplejson as json
+
+log = logging.getLogger(__name__)
+
+def getPostalFromFacebookLocation(location):
+    # need to take a string like "Santa Cruz, California" and get a postal code out of it
+    return '95060'
+
+def getFcbkPermissions():
+    return 'email'
+
+def facebookGraphAPIGetUsr(token):
+    """
+        Given an AccessToken gives the fcbk user
+    """
+    args = dict(access_token=token)
+    
+    try :
+        profile = load(urlopen("https://graph.facebook.com/me?"+
+                    urlencode(args)))
+        return profile
+    except:
+        print "Could not Get USer info"
+        return None
+
+class FloginController(BaseController):
+
+    def checkEmail(self, id1):
+        # this receives an email from the fb javascript auth checker, figures out what to do
+
+        # is there a user with this email?
+        user = userLib.getUserByEmail( id1 )
+        if user: # not none or false
+            logUserIn(self, user)
+            # if so, is it linked to an fb account by this email yet?
+            # ! seeIfUserFbAuth
+            
+            # ! here is where I either tap into the normal login flow, or 
+            # tap into the signup/registration/activation flow
+
+            # will need to figure out if I have ot do anything to pass along the 'afterLoginURL'
+            # - just look at how people can land on the site from invites, and press this fb login
+            # button instead of pressing the normal signup or login link.
+
+            #if so, log in
+            # ! go look at how the login controller logs you in
+            # likely a redirect to the workshop page
+        # from geo fx
+        #return json.dumps({'statusCode':statusCode, 'cityTitle':cityTitle, 'cityURL':cityURL, 'stateTitle':stateTitle, 'stateURL':stateURL, 'countryTitle':countryTitle, 'countryURL':countryURL})
+        if not user:
+            return 'user %s not found'%id1
+        return str( json.dumps({'user name':user['name'], 'user email':user['email']}) )
+
+
+
+    def loginHandler(self, id1):
+        log.info("loginHandler accessed")
+        log.info("id1: "+id1)
+        for key in request.environ:
+            log.info("req: "+str(request.environ[key]))
+        #log.info("token: "+token)
+        #log.info("code: "+code)
+
+        raise Exception('need to dig into the request response')
+        """http://todd.civinomics.org/flogin/loginHandler/a#
+        access_token=CAAB52LI5y94BAPOGLsrXnfJuH9XcINbilYE3LNosiA1ZBxdE7os4ShQaRmKlS5IeEh0BjYWV5orQ4eslduyjCXN0vebbpNwYVp1NtKMW2YQFFjXkerfzspV4L7ZBDaDuUT0Nc725x6ikyBbPWu
+        &
+        expires_in=5177474
+        &
+        code=AQBNIgLZoY-_6-YTzD_yA1nfZFlgYJCqLf4XPBG1etcIEyA3-MPgljdxxryHz_1r0P4yjdYY57dYYqURAA-NPgfbs4SvbEwoUpjiViv7jDrA2RsFlp8p3tRS2EyUJF2kScgUlrC74NUW-XYs-X3OrnKs0o61CayjSfIDRJFCZFn4NTdXtMrvtl6837p7nXnL3B3QtpIEnjkBtT7z2YUeZe-pBYTJsTYcPSxCKgf7SianFH0Nr9nygaPcSILMEVYguAA0oq_doaAVaHEBXJE7vpWDHqgcPfrSMaYcqAz5FTsIeO6ds9P82VaMBbwTWE_qnok
+        """
+
+        if 'error' in request.params:
+            wError = request.params['error']
+            if wError == 'error':
+                # there has been an error
+                if 'error_reason' in request.params:
+                    wErrorReason = request.params['error_reason']
+                if 'error_description' in request.params:
+                    wErrorDescription = request.params['error_description']
+                splashMsg['content'] = wErrorReason+'. '+wErrorDescription
+                session['splashMsg'] = splashMsg
+                session.save()
+                return redirect("/login")
+        if 'code' in request.params:
+            wCode = request.params['code']
+            log.info("code: "+wCode)
+        if 'token' in request.params:
+            wToken = request.params['token']
+            log.info("token: "+wToken)
+        if 'afterLoginURL' in session:
+            # look for accelerator cases: workshop home, item listing, item home
+            loginURL = session['afterLoginURL']
+            session.pop('afterLoginURL')
+            session.save()
+        else:
+            loginURL = "/"
+        
+        return redirect(loginURL)
+
+    def login(self):
+        args=dict(client_id=config['facebook.appid'], 
+                  #display='page',
+                  response_type='code token',
+                  redirect_uri=config['facebook.loginHandler'],
+                  #fbconnect=1,
+                  scope=getFcbkPermissions())
+        log.info("fb redirect: "+config['facebook.loginHandler'])
+        #return redirect("https://www.facebook.com/dialog/permissions.request?"+urlencode(args))
+        return redirect("https://www.facebook.com/dialog/oauth?"+urlencode(args))
+        """ This also has the following optional parameters:
+         - state 
+          An arbitrary unique string created by your app to guard against Cross-site Request Forgery
+         - response_type 
+          Determines whether the response data included when the redirect back to the app occurs 
+          is in URL parameters or fragments. See the Confirming Identity section to choose which 
+          type your app should use. This can be one of:
+           - code 
+            Response data is included as URL parameters and contains code parameter 
+            (an encrypted string unique to each login request). This is the default behaviour 
+            if this parameter is not specified.
+           - token 
+            Response data is included as a URL fragment and contains an access token
+           - code%20token
+            Response data is included as a URL fragment and contains both an access token and the 
+            code parameter.
+         - scope 
+          A comma separated list of Permissions to request from the person using your app
+        """
+    
+    def error(self):
+        return redirect(ROOT)
+    
+    def getAccessToken(self, code):
+        """
+        Given a facebook code , returns an auth token
+        """
+        args=dict(client_id=FACEBOOK_APP_ID,
+                  client_secret=FACEBOOK_SECRET,
+                  redirect_uri=request.path_url,
+                  code=code)
+        response = urlopen('https://graph.facebook.com/oauth/access_token?'+urlencode(args))
+        response_txt = response.read()
+        return parse_qs(response_txt)
+
+    def processingError(self, error):
+        """ 
+            Function to handle processing errors 
+            TODO: Currently returns error
+            But in future we need to redirect the user
+            to a webpage that is more informative
+        """
+        return error
+
+    def index(self):
+        if (request.params.get('error', None) != None) :
+            return self.error()
+        if (request.params.get('code', None) == None) :
+            return self.login()
+        else :
+            code = request.params.get('code')
+            accesshash = self.getAccessToken(
+                        request.params.get('code', None))
+            isAccessTokenPresent = 'access_token' in accesshash
+            if (isAccessTokenPresent == False) :
+                return self.processingError('Error accessing Faceboook validation')
+            else :
+                usr = facebookGraphAPIGetUsr(str(accesshash['access_token'][0]))
+                if (usr is None) :
+                    return self.processingError('Error accessing Facebook '\
+                                                 'validation')
+                else :
+                    buf = "<body><h1> Wie Gehst " +usr['name']+"? </h1>"
+                    buf = buf + "id :"+ usr['id']+\
+                          "<br> name :"+usr['name']+\
+                          "<br> email:"+usr['email']+\
+                          "<br>3~"
+                    return buf
+
+
+
---
+++ pylowiki/lib/db/facilitator.py
@@ -0,0 +1,171 @@
+#-*- coding: utf-8 -*-
+import logging
+import pickle
+
+from pylons import session, tmpl_context as c
+from pylowiki.model import Thing, meta
+from dbHelpers import commit, with_characteristic as wc
+import pylowiki.lib.db.generic      as generic
+import pylowiki.lib.utils           as utils
+
+log = logging.getLogger(__name__)
+
+# Getters
+def isFacilitator( user, workshop ):
+   f = meta.Session.query(Thing).filter_by(objType = 'facilitator').filter_by(owner = user.id).filter(Thing.data.any(wc('workshopCode', workshop['urlCode']))).filter(Thing.data.any(wc('disabled', '0'))).filter(Thing.data.any(wc('pending', '0'))).all()
+   if f:
+      return True
+   else:
+      return False
+
+def getFacilitatorByCode(code):
+    try:
+        return meta.Session.query(Thing)\
+                .filter_by(objType = 'facilitator')\
+                .filter(Thing.data.any(wc('urlCode', code)))\
+                .one()
+    except:
+        return False
+
+def isPendingFacilitator( user, workshop ):
+   f = meta.Session.query(Thing).filter_by(objType = 'facilitator').filter_by(owner = user.id).filter(Thing.data.any(wc('workshopCode', workshop['urlCode']))).filter(Thing.data.any(wc('disabled', '0'))).filter(Thing.data.any(wc('pending', '1'))).all()
+   if f:
+      return True
+   else:
+      return False
+
+def getFacilitatorsByWorkshop( workshop, disabled = '0'):
+    try:
+        return meta.Session.query(Thing).filter_by(objType = 'facilitator').filter(Thing.data.any(wc('disabled', disabled))).filter(Thing.data.any(wc('workshopCode', workshop['urlCode']))).all()
+    except:
+        return False
+
+def getFacilitatorsByUser(user, disabled = '0'):
+    try:
+        return meta.Session.query(Thing).filter_by(objType = 'facilitator').filter_by(owner = user.id).filter(Thing.data.any(wc('disabled', disabled))).all()
+    except:
+        return False
+        
+def setFacilitatorsByUserInSession(fdisabled = '0'):
+    if 'facilitatorWorkshops' not in c.authuser or 'facilitatorInitatives' not in c.authuser:
+        facilitatorList = getFacilitatorsByUser(c.authuser, disabled = fdisabled)
+        facilitatorWorkshops = []
+        facilitatorInitiatives = []
+        
+        for f in facilitatorList:
+            if f['disabled'] == fdisabled:
+                if 'workshopCode' in f:
+                    facilitatorWorkshops.append(f['workshopCode'])
+                elif 'initiativeCode' in f:
+                    facilitatorInitiatives.append(f['initiativeCode'])
+        c.authuser['facilitatorWorkshops'] = str(pickle.dumps(facilitatorWorkshops))
+        c.authuser['facilitatorInitiatives'] = str(pickle.dumps(facilitatorInitiatives))
+        
+    else:
+        facilitatorWorkshops = pickle.loads(str(c.authuser["facilitatorWorkshops"]))
+        facilitatorInitiatives = pickle.loads(str(c.authuser["facilitatorInitiatives"]))
+        
+    session["facilitatorWorkshops"] = facilitatorWorkshops
+    session["facilitatorInitiatives"] = facilitatorInitiatives
+    session.save()
+
+def getFacilitatorsByUserAndWorkshop(user, workshop, disabled = '0'):
+    try:
+        return meta.Session.query(Thing).filter_by(objType = 'facilitator').filter_by(owner = user.id).filter(Thing.data.any(wc('workshopCode', workshop['urlCode']))).filter(Thing.data.any(wc('disabled', disabled))).all()
+    except:
+        return False
+
+def getAllFacilitatorsByInitiative(initiative):
+    try:
+        return meta.Session.query(Thing).filter_by(objType = 'facilitator').filter(Thing.data.any(wc('initiativeCode', initiative['urlCode']))).all()
+    except:
+        return False
+
+def getFacilitatorsByInitiative(initiative, disabled = '0'):
+    try:
+        return meta.Session.query(Thing).filter_by(objType = 'facilitator').filter(Thing.data.any(wc('disabled', disabled))).filter(Thing.data.any(wc('initiativeCode', initiative['urlCode']))).all()
+    except:
+        return False
+
+def getFacilitatorsByUserAndInitiative(user, item, disabled = '0'):
+    try:
+        return meta.Session.query(Thing).filter_by(objType = 'facilitator').filter_by(owner = user.id).filter(Thing.data.any(wc('initiativeCode', item['urlCode']))).filter(Thing.data.any(wc('disabled', disabled))).all()
+    except:
+        return False
+
+def getFacilitatorInWorkshop(user, workshop):
+    try:
+        return meta.Session.query(Thing)\
+            .filter_by(objType = 'facilitator')\
+            .filter_by(owner = user.id)\
+            .filter(Thing.data.any(wc('workshopCode', workshop['urlCode'])))\
+            .one()
+    except:
+        return False
+
+# Setters
+def disableFacilitator( facilitator ):
+    """disable this facilitator"""
+    facilitator['disabled'] = '1'
+    commit(facilitator)
+    user = generic.getThingByID(facilitator.owner)
+    if 'facilitator_counter' in user:
+        fValue = int(user['facilitator_counter'])
+        fValue -= 1
+        user['facilitator_counter'] = str(fValue)
+    else:
+        user['facilitator_counter'] = '0'
+
+    if 'workshopCode' in facilitator:
+        facilitatorWorkshops = pickle.loads(str(user["facilitatorWorkshops"]))
+        facilitatorWorkshops.append(facilitator['workshopCode'])
+        user['facilitatorWorkshops'] = str(pickle.dumps(facilitatorWorkshops))
+        session['facilitatorWorkshops'] = facilitatorWorkshops
+    elif 'initiativeCode' in facilitator:
+        facilitatorInitiatives = pickle.loads(str(user["facilitatorInitiatives"]))
+        facilitatorWorkshops.append(facilitator['initiativeCode'])
+        facilitatorInitiatives.append(f['initiativeCode'])
+        session['facilitatorInitiatives'] = facilitatorInitiatives
+    commit(user)
+    session.save()
+
+
+def enableFacilitator( facilitator ):
+    """enable the facilitator"""
+    facilitator['disabled'] = '0'
+    commit(facilitator)
+    user = generic.getThingByID(facilitator.owner)
+    if 'facilitator_counter' in user:
+        fValue = int(user['facilitator_counter'])
+        fValue += 1
+        user['facilitator_counter'] = str(fValue)
+    else:
+        user['facilitator_counter'] = '1'
+
+    if 'workshopCode' in facilitator:
+        facilitatorWorkshops = pickle.loads(str(user["facilitatorWorkshops"]))
+        facilitatorWorkshops.append(facilitator['workshopCode'])
+        user['facilitatorWorkshops'] = str(pickle.dumps(facilitatorWorkshops))
+        session['facilitatorWorkshops'] = facilitatorWorkshops
+    elif 'initiativeCode' in facilitator:
+        facilitatorInitiatives = pickle.loads(str(user["facilitatorInitiatives"]))
+        facilitatorInitiatives.append(facilitator['initiativeCode'])
+        user['facilitatorInitiatives'] = str(pickle.dumps(facilitatorInitiatives))
+        session['facilitatorInitiatives'] = facilitatorInitiatives
+    commit(user)
+    session.save()
+
+# Object
+class Facilitator(object):
+    def __init__(self, user, workshop, pending = '0'):
+        # note - the userID of the facilitator is the f.owner
+        f = Thing('facilitator', user.id)
+        f['disabled'] = u'0'
+        f['pending'] = pending
+        f['itemAlerts'] = u'1'
+        f['flagAlerts'] = u'1'
+        f['digest'] = u'0'
+        generic.linkChildToParent(f, workshop)
+        commit(f)
+        f['urlCode'] = utils.toBase62(f)
+        commit(f)
