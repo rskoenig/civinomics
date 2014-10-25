3ca80e39f6484423691040cedba5a6a2f1459dfb
manu <manu@civinomics.com>
2014-10-24 17:29:55 -0700

    added link to comment in email messages; comments on discussions, resources and ideas also generate emails

---
+++ pylowiki/public/js/ng/showThing.js
@@ -0,0 +1,67 @@
+function showThingCtrl($scope, $http, $location, $anchorScroll) {
+	$scope.costRegex = /^(\-)?(([1-9]\d{0,2}(,\d{3})*)|(([1-9]\d*)?\d))$/;
+    $scope.clearTitle = function() {
+        if ($scope.initiativeTitle == 'New Initiative'){
+            $scope.initiativeTitle = '';
+        }
+    };
+
+    $scope.scrollTo = function(id) {
+      $location.hash(id);
+      $anchorScroll();
+   	}
+
+    $scope.getUrl = '/' + $scope.objType + '/' + $scope.thingCode + '/' + $scope.thingUrl + '/json';
+
+    $scope.getThingData = function(){
+    	$http.get($scope.getUrl).success(function(data){
+			if (data.statusCode === 0){
+				$scope.noResult = true;
+			}
+			else if (data.statusCode == 1){
+				$scope.noResult = false;
+				$scope.item = data.thing;
+
+				$scope.summary = $scope.item.text;
+				if ($scope.summary) {
+					$scope.getWordCount()
+				};
+				$scope.urlCode= $scope.item.urlCode;
+				$scope.url= $scope.item.url;
+				$scope.totalVotes= $scope.item.voteCount;
+				$scope.yesVotes= $scope.item.ups;
+				$scope.noVotes= $scope.item.downs;
+				$scope.yesPercent = $scope.yesVotes / $scope.totalVotes * 100;
+        		$scope.noPercent = $scope.noVotes / $scope.totalVotes * 100;
+				$scope.objType= $scope.item.objType;
+				$scope.rated= $scope.item.rated;
+				$scope.goal= $scope.item.goal;
+				if ($scope.rated == 0) {
+			        $scope.voted = '';
+			    }
+			    else if ($scope.rated == 1){
+			        $scope.voted = 'yesVoted';
+			    }
+			    else if ($scope.rated == -1){
+			        $scope.voted = 'noVoted';
+			    }
+
+			}
+			$scope.loading = false;
+		});
+    };
+
+    $scope.getThingData();
+
+    if ($scope.summary) {
+		$scope.wordCount = $scope.summary.trim().split(/\s+/).length;
+   	};
+	$scope.getWordCount = function() {
+   		if ($scope.summary == ''){
+   			$scope.wordCount = 0;
+   		} else{
+        	$scope.wordCount = $scope.summary.trim().split(/\s+/).length;
+        };
+	}   	
+
+}
