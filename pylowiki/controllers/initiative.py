8511d721b33ad5c87691903d4b90fa0873df0859
Maria Lopez-Latorre <mentatzoe@gmail.com>
2014-11-06 14:24:16 -0800

    Merge conflict fix

--- pylowiki/controllers/initiative.py
+++ pylowiki/controllers/initiative.py
@@ -92,20 +92,19 @@ class InitiativeController(BaseController):
 
         # only the author or an admin can edit 
         c.iPrivs = False
-        if 'workshop_subcategory_tags' in c.initiative and c.initiative['workshop_subcategory_tags'] is not None:
-            log.info(c.initiative['workshop_subcategory_tags'])
-            if 'subcategory_tags' in c.initiative and c.initiative['subcategory_tags'] is not None:
-                log.info(c.initiative['subcategory_tags'])
-                tempList = c.initiative['subcategory_tags'].split("|")
-                for tag in tempList:
-                    if tag not in c.initiative['workshop_subcategory_tags'].split("|"):
-                        tempList.remove(tag)
-                if len(tempList) == 0 :
-                    c.initiative['subcategory_tags'] = False
-                else:
-                    c.initiative['subcategory_tags'] = "|".join(tempList)
-        else:
-            log.info("Nope")
+        if c.initiative:
+            if 'workshop_subcategory_tags' in c.initiative and c.initiative['workshop_subcategory_tags'] is not None:
+                if 'subcategory_tags' in c.initiative and c.initiative['subcategory_tags'] is not None:
+                    tempList = c.initiative['subcategory_tags'].split("|")
+                    for tag in tempList:
+                        if tag not in c.initiative['workshop_subcategory_tags'].split("|"):
+                            tempList.remove(tag)
+                    if len(tempList) == 0 :
+                        c.initiative['subcategory_tags'] = False
+                    else:
+                        c.initiative['subcategory_tags'] = "|".join(tempList)
+            else:
+                log.info("Nope")
             
         facilitator = False
         f = facilitatorLib.getFacilitatorsByUserAndInitiative(c.authuser, c.initiative)
@@ -798,6 +797,3 @@ class InitiativeController(BaseController):
     def getJson(self):
         entry = jsonLib.getJsonProperties(c.initiative)
         return json.dumps({'statusCode':1, 'thing': entry})
-
-        
-            
