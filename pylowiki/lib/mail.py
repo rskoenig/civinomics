9e6283cac67104fc105cea3b723ff54f4bc96337
manu <manu@civinomics.com>
2014-11-11 13:42:40 -0800

    fix for comment listing on geo-profiles

---
+++ pylowiki/lib/mail.py
@@ -0,0 +1,296 @@
+import os
+import logging
+import pylowiki.lib.utils           as utils
+from pylons import config, request
+
+log = logging.getLogger(__name__)
+
+def send( send_too, send_from, subject, message ):
+
+    #SENDMAIL = "sendmail" #path to sendmail
+    SENDMAIL = "/usr/sbin/sendmail" #path to sendmail
+    BR = "\n" # BR types/reads easier
+
+    sub_process = os.popen( "%s -t" % SENDMAIL, "w" )
+    sub_process.write( "To: " + send_too + BR )
+    sub_process.write( "From: " + send_from + BR )
+    sub_process.write( "Subject: " + subject + BR )
+    sub_process.write( BR ) #blank line seperates headers from body
+    sub_process.write( message + BR )
+    status = sub_process.close()
+    return True
+
+def sendWelcomeMail(u): 
+    emailDir = config['app_conf']['emailDirectory']
+    txtFile = emailDir + "/welcome.txt"
+
+    # open and read the text file
+    fp = open(txtFile, 'r')
+    textMessage = fp.read()
+    fp.close()
+
+    subject = "Civinomics: let's get started!"
+    fromEmail = config['activation.email']
+    toEmail = u['email']
+    
+    send(toEmail, fromEmail, subject, textMessage)
+    
+       
+def sendActivationMail(recipient, activationLink):
+    subject = 'Civinomics Account Activation'
+    
+    emailDir = config['app_conf']['emailDirectory']
+    txtFile = emailDir + "/activate.txt"
+
+    # open and read the text file
+    fp = open(txtFile, 'r')
+    textMessage = fp.read()
+    fp.close()
+    
+    # do the substitutions
+    textMessage = textMessage.replace('${c.activationLink}', activationLink)
+
+    fromEmail = config['activation.email']
+    toEmail = recipient
+
+    send(toEmail, fromEmail, subject, textMessage)
+    
+def sendActivationMailWithPassword(recipient, activationLink, password):
+    subject = 'Civinomics Account Activation'
+    
+
+    emailDir = config['app_conf']['emailDirectory']
+    txtFile = emailDir + "/activateWithPassword.txt"
+
+    # open and read the text file
+    fp = open(txtFile, 'r')
+    textMessage = fp.read()
+    fp.close()
+    textMessage = textMessage.replace('${c.activationLink}', activationLink)
+    textMessage = textMessage.replace('${password}', password)
+    
+    fromEmail = config['activation.email']
+    toEmail = recipient
+
+    send(toEmail, fromEmail, subject, textMessage)
+
+       
+def sendPMemberInvite(workshopName, senderName, recipient, message, browseURL):
+    
+    emailDir = config['app_conf']['emailDirectory']
+    txtFile = emailDir + "/private_invite.txt"
+    
+    if 'paste.testing_variables' in request.environ:
+            request.environ['paste.testing_variables']['browseUrl'] = browseURL
+   
+    # open and read the text file
+    fp = open(txtFile, 'r')
+    textMessage = fp.read()
+    fp.close()
+    
+    # do the substitutions
+    textMessage = textMessage.replace('${c.sender}', senderName)
+    textMessage = textMessage.replace('${c.workshopName}', workshopName)
+    textMessage = textMessage.replace('${c.inviteMessage}', message)
+    textMessage = textMessage.replace('${c.browseLink}', browseURL)
+    textMessage = textMessage.replace('${c.browseLink}', browseURL)
+    fromEmail = 'Civinomics Invitations <invitations@civinomics.com>'
+    toEmail = recipient
+    subject = 'An invitation from ' + senderName
+
+    send(toEmail, fromEmail, subject, textMessage)
+    
+def sendWorkshopMail(recipient):    
+    emailDir = config['app_conf']['emailDirectory']
+    myURL = config['app_conf']['site_base_url']
+
+    txtFile = emailDir + "/workshop.txt"
+    
+    # open and read the text file
+    fp = open(txtFile, 'r')
+    textMessage = fp.read()
+    fp.close()
+
+    subject = 'About your new Civinomics workshop'
+    fromEmail = 'Civinomics Helpdesk <helpdesk@civinomics.com>'
+    toEmail = recipient
+
+    send(toEmail, fromEmail, subject, textMessage)
+    
+def sendAccountMail(recipient):
+           
+    subject = 'Information about your new Civinomics Professional Workshop account'
+    
+    emailDir = config['app_conf']['emailDirectory']
+    txtFile = emailDir + "/account.txt"
+
+    # open and read the text file
+    fp = open(txtFile, 'r')
+    textMessage = fp.read()
+    fp.close()
+
+    fromEmail = 'Civinomics Billing <billing@civinomics.com>'
+
+    send(recipient, fromEmail, subject, textMessage)
+
+def sendCommentMail(recipient, sender, parent, dparent, comment):
+           
+    subject = 'Civinomics Alert: A new comment has been added to one of your items'
+    fromEmail = 'Civinomics Alerts <alerts@civinomics.com>'
+    
+    textMessage = sender['name'] + " has commented on your " + parent.objType.replace("Unpublished", "") 
+    if parent.objType != 'comment':
+        textMessage += ' "' + parent['title'] + '"'
+    else:
+        textMessage += '"' + parent['data'] + '"'
+        
+    if 'workshopCode' in parent:
+        parentBase = "workshop"
+        textMessage += ' in the ' + parentBase + ' titled "' + dparent['title'] + '"'
+
+    textMessage += ': civ.io/' + parent.objType.replace("Unpublished", "") + '/' + parent['urlCode'] + '/' + parent['url'] + '#comment-' + comment['urlCode']
+    textMessage += '\n\n"' + comment['data'] + '"\n\n'
+    textMessage += "\n\nThis is an automated message. Your Civinomics profile preferences are set to email \nnotifications when someone comments on one of your items.\nTo change this, login to your Civinomics account, and go to the Preferences menu\nof your Edit Profile tab." 
+
+    send(recipient, fromEmail, subject, textMessage)
+    
+def sendListenerInviteMail(recipient, user, workshop, memberMessage, numInvites):
+           
+    subject = 'You are invited to listen in on a Civinomics workshop'
+    
+    emailDir = config['app_conf']['emailDirectory']
+    txtFile = emailDir + "/invitelistener.txt"
+    browseURL = config['app_conf']['site_base_url'] + "/workshop" + "/" + workshop['urlCode'] + "/" + workshop['url']
+    if numInvites == '0':
+        numInvitesMessage = ''
+    else:
+        numInvitesMessage = "\nThere are now %s members who've asked you to join in!\n"%numInvites
+
+    # open and read the text file
+    fp = open(txtFile, 'r')
+    textMessage = fp.read()
+    fp.close()
+    
+    textMessage = textMessage.replace('${c.sender}', user['name'])
+    textMessage = textMessage.replace('${c.workshop}', workshop['title'])
+    textMessage = textMessage.replace('${c.browseLink}', browseURL)
+    textMessage = textMessage.replace('${c.memberMessage}', memberMessage)
+    textMessage = textMessage.replace('${c.numInvitesMessage}', numInvitesMessage)
+
+    fromEmail = 'Civinomics Invitations <invitations@civinomics.com>'
+
+    send(recipient, fromEmail, subject, textMessage)
+    
+def sendListenerAddMail(recipient, user, workshop):
+           
+    subject = 'You are invited to listen in on a Civinomics workshop'
+    
+    emailDir = config['app_conf']['emailDirectory']
+    txtFile = emailDir + "/addlistener.txt"
+    browseURL = config['app_conf']['site_base_url'] + "/workshop" + "/" + workshop['urlCode'] + "/" + workshop['url']
+
+    # open and read the text file
+    fp = open(txtFile, 'r')
+    textMessage = fp.read()
+    fp.close()
+    
+    textMessage = textMessage.replace('${c.sender}', user['name'])
+    textMessage = textMessage.replace('${c.workshop}', workshop['title'])
+    textMessage = textMessage.replace('${c.browseLink}', browseURL)
+
+    fromEmail = 'Civinomics Invitations <invitations@civinomics.com>'
+
+    send(recipient, fromEmail, subject, textMessage)
+    
+def sendNewListenerMail(listener):
+           
+    subject = 'You have been added as a Civinomics Listener'
+    
+    emailDir = config['app_conf']['emailDirectory']
+    txtFile = emailDir + "/newlistener.txt"
+    scopeInfo = utils.getPublicScope(listener)
+    listenerScope = scopeInfo['level'] + " of " + scopeInfo['name']
+    
+    # 0|0|united-states|0|0|0|0|0|0|0
+    scopeList = listener['scope'].split('|')
+    state = scopeList[4]
+    county = scopeList[6]
+    city = scopeList[8]
+    
+    browseURL = config['app_conf']['site_base_url'] + "/workshops/geo/earth/united-states"
+    
+    if state != '0' and state != '':
+        browseURL += '/' + state
+    if county != '0' and county != '':
+        browseURL += '/' + county
+    if city != '0' and city != '':
+        browseURL += '/' + city
+
+
+    # open and read the text file
+    fp = open(txtFile, 'r')
+    textMessage = fp.read()
+    fp.close()
+    
+    textMessage = textMessage.replace('${c.listenerScope}', listenerScope)
+    textMessage = textMessage.replace('${c.listenerName}', listener['name'])
+    textMessage = textMessage.replace('${c.browseLink}', browseURL)
+
+    fromEmail = 'Civinomics Invitations <invitations@civinomics.com>'
+
+    send(listener['email'], fromEmail, subject, textMessage)
+    
+def sendShareMail(recipientEmail, memberMessage, user, item, itemURL):
+           
+    subject = '%s shared "%s" with you' % (user['name'], item['title'])
+    
+    emailDir = config['app_conf']['emailDirectory']
+    txtFile = emailDir + "/shareemail.txt"
+    
+    introduction = ""
+    #itemDet = 'a'
+    #if item.objType == 'idea':
+    #    itemDet = 'an'
+        
+    introduction = "This %s is hosted on Civinomics:\n%s\n\n"%(item.objType, itemURL)
+
+    #if item.objType == 'workshop':
+    #   introduction += 'The workshop is titled "%s".'%item['title']
+    #else:
+    #    introduction += 'The %s is titled "%s" and is in a workshop titled "%s".'%(item.objType, item['title'], workshop['title'])
+
+    # open and read the text file
+    fp = open(txtFile, 'r')
+    textMessage = fp.read()
+    fp.close()
+    
+    #textMessage = textMessage.replace('${c.sender}', user['name'])
+    textMessage = textMessage.replace('${c.intro}', introduction)
+    textMessage = textMessage.replace('${c.memberMessage}', memberMessage)
+
+    fromEmail = 'Civinomics Invitations <invitations@civinomics.com>'
+
+    send(recipientEmail, fromEmail, subject, textMessage)
+
+
+def sendCoauthorAddMail(recipient, user, initiative):
+    log.info('mail controller called')
+           
+    subject = '%s invited you to coauthor %s' % (user['name'], initiative['title'])
+    
+    emailDir = config['app_conf']['emailDirectory']
+    txtFile = emailDir + "/addCoauthor.txt"
+    messagesURL = utils.getBaseUrl() + "/messages/" + recipient['urlCode'] + "/" + recipient['url']
+
+    # open and read the text file
+    fp = open(txtFile, 'r')
+    textMessage = fp.read()
+    fp.close()
+    
+    textMessage = textMessage.replace('${c.sender}', user['name'])
+    textMessage = textMessage.replace('${c.initiative}', initiative['title'])
+    textMessage = textMessage.replace('${c.messagesLink}', messagesURL)
+
+    fromEmail = 'Civinomics Invitations <invitations@civinomics.com>'
+
+    send(recipient['email'], fromEmail, subject, textMessage)
---
+++ pylowiki/model/__init__.py
@@ -0,0 +1,198 @@
+#from sqlalchemy import (MetaData, Table, Column, Integer, Unicode, DateTime,
+from sqlalchemy import (Table, Column, Integer, Unicode, DateTime, BLOB, 
+        ForeignKey, UnicodeText, and_, not_)
+from sqlalchemy.dialects.mysql import LONGTEXT
+from sqlalchemy.orm import mapper, relationship, Session
+from sqlalchemy.orm.collections import attribute_mapped_collection
+
+
+from pylowiki.model import meta
+
+import datetime as d
+
+#metadata = MetaData()
+
+def init_model(engine):
+    meta.Session.configure(bind=engine)
+    meta.engine = engine
+
+"""Mapping a vertical table as a dictionary.
+
+This example illustrates accessing and modifying a "vertical" (or
+"properties", or pivoted) table via a dict-like interface.  These are tables
+that store free-form object properties as rows instead of columns.  For
+example, instead of::
+
+  # A regular ("horizontal") table has columns for 'species' and 'size'
+  Table('animal', metadata,
+        Column('id', Integer, primary_key=True),
+        Column('species', Unicode),
+        Column('size', Unicode))
+
+A vertical table models this as two tables: one table for the base or parent
+entity, and another related table holding key/value pairs::
+
+  Table('animal', metadata,
+        Column('id', Integer, primary_key=True))
+
+  # The properties table will have one row for a 'species' value, and
+  # another row for the 'size' value.
+  Table('properties', metadata
+        Column('animal_id', Integer, ForeignKey('animal.id'),
+               primary_key=True),
+        Column('key', UnicodeText),
+        Column('value', UnicodeText))
+
+Because the key/value pairs in a vertical scheme are not fixed in advance,
+accessing them like a Python dict can be very convenient.  The example below
+can be used with many common vertical schemas as-is or with minor adaptations.
+
+"""
+
+class VerticalProperty(object):
+    """A key/value pair.
+
+    This class models rows in the vertical table.
+    """
+
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+
+    def __repr__(self):
+        return '<%s %r=%r>' % (self.__class__.__name__, self.key, self.value)
+
+
+class VerticalPropertyDictMixin(object):
+    """Adds obj[key] access to a mapped class.
+
+    This is a mixin class.  It can be inherited from directly, or included
+    with multiple inheritence.
+
+    Classes using this mixin must define two class properties::
+
+    _property_type:
+      The mapped type of the vertical key/value pair instances.  Will be
+      invoked with two positional arugments: key, value
+
+    _property_mapping:
+      A string, the name of the Python attribute holding a dict-based
+      relationship of _property_type instances.
+
+    Using the VerticalProperty class above as an example,::
+
+      class MyObj(VerticalPropertyDictMixin):
+          _property_type = VerticalProperty
+          _property_mapping = 'props'
+
+      mapper(MyObj, sometable, properties={
+        'props': relationship(VerticalProperty,
+                          collection_class=attribute_mapped_collection('key'))})
+
+    Dict-like access to MyObj is proxied through to the 'props' relationship::
+
+      myobj['key'] = 'value'
+      # ...is shorthand for:
+      myobj.props['key'] = VerticalProperty('key', 'value')
+
+      myobj['key'] = 'updated value']
+      # ...is shorthand for:
+      myobj.props['key'].value = 'updated value'
+
+      print myobj['key']
+      # ...is shorthand for:
+      print myobj.props['key'].value
+
+    """
+
+    _property_type = VerticalProperty
+    _property_mapping = None
+
+    __map = property(lambda self: getattr(self, self._property_mapping))
+
+    def __getitem__(self, key):
+        return self.__map[key].value
+
+    def __setitem__(self, key, value):
+        property = self.__map.get(key, None)
+        if property is None:
+            self.__map[key] = self._property_type(key, value)
+        else:
+            property.value = value
+
+    def __delitem__(self, key):
+        del self.__map[key]
+
+    def __contains__(self, key):
+        return key in self.__map
+
+    # Implement other dict methods to taste.  Here are some examples:
+    def keys(self):
+        return self.__map.keys()
+
+    def values(self):
+        return [prop.value for prop in self.__map.values()]
+
+    def items(self):
+        return [(key, prop.value) for key, prop in self.__map.items()]
+
+    def __iter__(self):
+        return iter(self.keys())
+
+
+# Here we have objects (things) and their properties (data)
+t_thing = Table('thing', meta.metadata,
+                Column('id', Integer, primary_key=True),
+                Column('objType', Unicode(100)),
+                Column('date', DateTime, default = d.datetime.now),
+                Column('owner', Integer),
+                Column('sort', Unicode(40), default = None))
+
+t_data = Table('data', meta.metadata,
+              Column('thing_id', Integer, ForeignKey('thing.id'),
+                     primary_key=True),
+              Column('key', Unicode(100), primary_key=True),
+              Column('value', LONGTEXT(convert_unicode=True, collation='utf8_bin'), default=None))
+
+t_blob = Table('blobbicus', meta.metadata,
+               Column('thing_id', Integer, ForeignKey('thing.id'), primary_key = True),
+               Column('key', Unicode(100), primary_key=True),
+               Column('value', BLOB, default = None))
+
+class Blob(VerticalProperty):
+    """ Used to store things like images, pdf files, etc... """
+
+class Data(VerticalProperty):
+    """thing properties"""
+
+class Thing(VerticalPropertyDictMixin):
+    """An object on the site.
+
+    thing properties are available via the 'data' property or by using
+    dict-like accessors on a Thing instance::
+
+      cat['color'] = 'calico'
+      # or, equivalently:
+      cat.data['color'] = Data('color', 'calico')
+    """
+
+    _property_type = Data
+    _property_mapping = 'data'
+
+    def __init__(self, objType, owner = u'0'):
+        self.objType = objType
+        self.owner = owner
+
+    def __repr__(self):
+        return '<%s %r>' % (self.__class__.__name__, self.objType)
+
+
+mapper(Thing, t_thing, properties={
+    'data': relationship(
+        Data, backref='thing',
+        collection_class=attribute_mapped_collection('key')),
+    'blob' : relationship(Blob, backref = 'thing', 
+        collection_class=attribute_mapped_collection('key')),
+    })
+mapper(Data, t_data)
+mapper(Blob, t_blob)
