ebf4b0440f52eecf5209ed3930839e473f539e9b
manu <manu@civinomics.com>
2014-11-20 15:58:49 -0800

    merged default into hotfix

---
+++ pylowiki/public/js/activate.js
@@ -0,0 +1,33 @@
+$('button.activateButton').click(function(e){
+    e.preventDefault();
+    $button = $(this);
+    var urlList = $button.attr('data-URL-list').split(/_/);
+    var urlString = '/activate/user/' + urlList[1];
+    if($button.hasClass('notactivated')){
+        $.ajax({
+           type : 'POST',
+           async : false,
+           url  : urlString
+        });
+        
+        $button.removeClass('notactivated');
+        var bText = '<span>User Activated</span>';
+        $button.html(bText);
+    } 
+});
+
+$('button.resendActivateEmailButton').click(function(e){
+    e.preventDefault();
+    $button = $(this);
+    var urlList = $button.attr('data-URL-list').split(/_/);
+    var urlString = '/activateResend/' + urlList[1];
+    var result = $.ajax({
+        type : 'POST',
+        async : false,
+        url  : urlString
+    });
+    var message = "Registration email resent! Close or refresh this page after clicking the emailed link." ;  
+    resendMessage = document.getElementById('resendMessage')
+    resendMessage.innerText = message;
+    resendMessage.className = "alert alert-success";
+});
