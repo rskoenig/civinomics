86fae3a1b7e85cadf1775b7da956d3bf52f1e574
Maria Lopez-Latorre <marialopezlatorre@gmail.com>
2015-01-07 15:28:26 -0800

    Reopening branch to close

---
+++ pylowiki/tests/functional/test_AreYouSure.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from pylowiki.tests import *
+
+class TestAreyousureController(TestController):
+    pass
+    """
+    def test_delete(self):
+        response = self.app.get(url(controller='AreYouSure', action='delete'))
+        # Test response...
+
+    def test_restore(self):
+        response = self.app.get(url(controller='AreYouSure', action='restore'))
+        # Test response...
+    """
